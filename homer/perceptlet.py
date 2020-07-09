7583d371e578bae9bfdb164400bbc01220b16d56
George Wright <george.alexander.wright@gmail.com>
2020-07-09 16:32:05 +0100

    implement perceptlet methods

--- homer/perceptlet.py
+++ homer/perceptlet.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from abc import ABC
 from typing import Any, List
 
@@ -10,10 +11,18 @@ class Perceptlet(ABC):
     def __init__(self, value: Any, neighbours: List[Any]):
         self.value = value
         self.neighbours = neighbours
-        self.labels = {}
+        self.labels = set()
 
-    def proportion_of_neighbours_with_label(self, concept: Concept):
-        pass
+    def proportion_of_neighbours_with_label(self, concept: Concept) -> float:
+        return self.number_of_neighbours_with_label(concept) / len(self.neighbours)
 
-    def add_label(self, concept: Concept, strength: float):
-        pass
+    def number_of_neighbours_with_label(self, concept: Concept) -> int:
+        return sum(1 for neighbour in self.neighbours if neighbour.has_label(concept))
+
+    def has_label(self, concept: Concept) -> bool:
+        return True in (
+            True for label in self.labels if label.parent_concept == concept
+        )
+
+    def add_label(self, label: Perceptlet) -> None:
+        self.labels.add(label)
---
+++ tests/unit/test_perceptlet.py
@@ -0,0 +1,47 @@
+import math
+import pytest
+from unittest.mock import Mock
+
+from homer.perceptlet import Perceptlet
+
+
+FLOAT_COMPARISON_TOLERANCE = 1e-5
+
+
+@pytest.fixture
+def concept():
+    mock_concept = Mock()
+    return mock_concept
+
+
+@pytest.fixture
+def label(concept):
+    mock_label = Mock()
+    mock_label.parent_concept = concept
+    return mock_label
+
+
+@pytest.mark.parametrize(
+    "no_of_valid_neighbours, no_of_invalid_neighbours, expected_proportion",
+    [(1, 1, 0.5), (1, 2, 0.33333), (0, 1, 0.0)],
+)
+def test_number_and_proportion_of_neighbours_with_label(
+    concept,
+    label,
+    no_of_valid_neighbours,
+    no_of_invalid_neighbours,
+    expected_proportion,
+):
+    valid_neighbour = Perceptlet("value", [])
+    valid_neighbour.labels.add(label)
+    invalid_neighbour = Perceptlet("value", [])
+    valid_neighbours = [valid_neighbour for i in range(no_of_valid_neighbours)]
+    invalid_neighbours = [invalid_neighbour for i in range(no_of_invalid_neighbours)]
+    neighbours = valid_neighbours + invalid_neighbours
+    print(neighbours)
+    perceptlet = Perceptlet("value", neighbours)
+    assert no_of_valid_neighbours == perceptlet.number_of_neighbours_with_label(concept)
+    actual_proportion = perceptlet.proportion_of_neighbours_with_label(concept)
+    assert math.isclose(
+        expected_proportion, actual_proportion, abs_tol=FLOAT_COMPARISON_TOLERANCE
+    )
