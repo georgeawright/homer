6590ff2b4ea1403453cc7dcd9e6fb4ff2a0111c7
George Wright <george.alexander.wright@gmail.com>
2020-07-10 16:15:01 +0100

    add add and remove perceptlet functions to worldview

--- homer/worldview.py
+++ homer/worldview.py
@@ -1,8 +1,14 @@
-from typing import List
+from typing import Set
 
 from homer.perceptlet import Perceptlet
 
 
 class Worldview:
-    def __init__(self, perceptlets: List[Perceptlet]):
+    def __init__(self, perceptlets: Set[Perceptlet]):
         self.perceptlets = perceptlets
+
+    def add_perceptlet(self, perceptlet: Perceptlet):
+        self.perceptlets.add(perceptlet)
+
+    def remove_perceptlet(self, perceptlet: Perceptlet):
+        self.perceptlets.remove(perceptlet)
