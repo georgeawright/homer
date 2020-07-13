11139718681b6b92db2598094509305c43e5345d
George Wright <george.alexander.wright@gmail.com>
2020-07-13 17:28:30 +0100

    implement group size, importance, and unhappiness

--- homer/perceptlets/group.py
+++ homer/perceptlets/group.py
@@ -1,10 +1,18 @@
+from __future__ import annotations
 from typing import List
 
+import statistics
+
+from homer.hyper_parameters import HyperParameters
 from homer.perceptlet import Perceptlet
+from homer.perceptlets.relation import Relation
 
 
 class Group(Perceptlet):
     """A grouping of other perceptlets."""
+
+    SIZE_IMPORTANCE_WEIGHT = HyperParameters.SIZE_IMPORTANCE_WEIGHT
+    LABEL_IMPORTANCE_WEIGHT = HyperParameters.LABEL_IMPORTANCE_WEIGHT
 
     def __init__(self, members: List[Perceptlet], strength: float):
         value = "?"
@@ -12,3 +20,45 @@ class Group(Perceptlet):
         Perceptlet.__init__(self, value, neighbours)
         self.members = members
         self.strength = strength
+        self.groups = set()
+        self.relations = set()
+
+    @property
+    def size(self) -> int:
+        return sum(
+            member.size if type(member) == Group else 1 for member in self.members
+        )
+
+    @property
+    def importance(self) -> float:
+        return statistics.fmean(
+            [
+                self._size_based_importance * self.SIZE_IMPORTANCE_WEIGHT,
+                self._label_based_importance * self.LABEL_IMPORTANCE_WEIGHT,
+            ]
+        )
+
+    @property
+    def _size_based_importance(self) -> float:
+        return 1.0 - 1.0 / self.size
+
+    @property
+    def _label_based_importance(self) -> float:
+        total_label_strengths = sum(label.strength for label in self.labels)
+        total_label_strengths_inverse = 1.0 / (1.0 + total_label_strengths)
+        return 1.0 - total_label_strengths_inverse
+
+    @property
+    def unhappiness(self) -> float:
+        total_connections = len(self.labels) + len(self.relations) + len(self.groups)
+        print(total_connections)
+        try:
+            return 1.0 / total_connections
+        except ZeroDivisionError:
+            return 1.0
+
+    def add_group(self, group: Group):
+        self.groups.add(group)
+
+    def add_relation(self, relation: Relation):
+        self.relations.add(relation)
---
+++ tests/unit/perceptlets/test_group.py
@@ -0,0 +1,78 @@
+import math
+import pytest
+from unittest.mock import Mock
+
+from homer.perceptlets.group import Group
+
+
+FLOAT_COMPARISON_TOLERANCE = 1e-3
+
+
+def test_size_flat_group():
+    expected_group_size = 30
+    members = [Mock() for _ in range(expected_group_size)]
+    group = Group(members, Mock())
+    assert expected_group_size == group.size
+
+
+def test_size_recursive_group():
+    group_depth = 30
+    group = Group([Mock()], Mock())
+    for _ in range(group_depth):
+        group = Group([group], Mock())
+    assert 1 == group.size
+
+
+@pytest.mark.parametrize(
+    "group_size, expected_importance", [(1, 0), (2, 0.5), (10, 0.9), (100, 0.99)]
+)
+def test_size_based_importance(group_size, expected_importance):
+    members = [Mock() for _ in range(group_size)]
+    group = Group(members, Mock())
+    assert math.isclose(
+        expected_importance,
+        group._size_based_importance,
+        abs_tol=FLOAT_COMPARISON_TOLERANCE,
+    )
+
+
+@pytest.mark.parametrize(
+    "label_strengths, expected_importance",
+    [
+        ([], 0),
+        ([0.2, 0.3], 0.333),
+        ([0.7, 0.8], 0.6),
+        ([1, 1], 0.667),
+        ([1, 1, 1, 1], 0.8),
+    ],
+)
+def test_label_based_importance(label_strengths, expected_importance):
+    group = Group([], [])
+    for label_strength in label_strengths:
+        label = Mock()
+        label.strength = label_strength
+        group.add_label(label)
+    assert math.isclose(
+        expected_importance,
+        group._label_based_importance,
+        abs_tol=FLOAT_COMPARISON_TOLERANCE,
+    )
+
+
+@pytest.mark.parametrize(
+    "number_of_labels, number_of_groups, number_of_relations, expected_unhappiness",
+    [(0, 0, 0, 1.0), (1, 0, 0, 1.0), (1, 1, 1, 0.333), (2, 3, 0, 0.2)],
+)
+def test_unhappiness(
+    number_of_labels, number_of_groups, number_of_relations, expected_unhappiness
+):
+    group = Group("value", [])
+    for i in range(number_of_labels):
+        group.add_label(Mock())
+    for i in range(number_of_groups):
+        group.add_group(Mock())
+    for i in range(number_of_relations):
+        group.add_relation(Mock())
+    assert math.isclose(
+        expected_unhappiness, group.unhappiness, abs_tol=FLOAT_COMPARISON_TOLERANCE
+    )
