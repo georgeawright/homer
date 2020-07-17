5a7b5c65e35b0404c60ed0a157e4faef5c508ffd
George Wright <george.alexander.wright@gmail.com>
2020-07-17 12:14:48 +0100

    reimplement classifier with fuzzy logic gates

--- homer/codelets/top_down_classifier_codelet.py
+++ homer/codelets/top_down_classifier_codelet.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from homer import fuzzy
 from homer.bubble_chamber import BubbleChamber
 from homer.codelet import Codelet
 from homer.concept import Concept
@@ -16,25 +17,21 @@ class TopDownClassifierCodelet(Codelet):
         bubble_chamber: BubbleChamber,
         parent_concept: Concept,
         target_perceptlet: Optional[Perceptlet],
-        classification_weights,
         urgency: float,
     ):
         Codelet.__init__(self, bubble_chamber)
         self.parent_concept = parent_concept
         self.target_perceptlet = target_perceptlet
-        self.classification_weights = classification_weights
         self.urgency = urgency
 
     def run(self) -> Optional[Codelet]:
-        confidence_of_class_membership = self.calculate_confidence(
-            [
-                self.parent_concept.activation,
-                self.parent_concept.depth,
-                self.parent_concept.distance_from(self.target_perceptlet.value),
-                self.target_perceptlet.proportion_of_neighbours_with_label(
-                    self.parent_concept
-                ),
-            ]
+        confidence_of_class_membership = self._calculate_confidence(
+            self.parent_concept.activation,
+            self.parent_concept.depth_rating,
+            self.parent_concept.distance_rating(self.target_perceptlet.value),
+            self.target_perceptlet.proportion_of_neighbours_with_label(
+                self.parent_concept
+            ),
         )
         self.parent_concept.boost_activation(confidence_of_class_membership)
         if confidence_of_class_membership > self.CONFIDENCE_THRESHOLD:
@@ -45,14 +42,19 @@ class TopDownClassifierCodelet(Codelet):
             return self.engender_follow_up(confidence_of_class_membership)
         return None
 
-    def calculate_confidence(self, inputs) -> float:
-        return sum(i[0] * i[1] for i in zip(inputs, self.classification_weights))
+    def _calculate_confidence(
+        self,
+        concept_activation: float,
+        concept_depth: float,
+        distance_from_prototype: float,
+        proportion_of_neighbours: float,
+    ) -> float:
+        return fuzzy.AND(
+            fuzzy.NAND(concept_depth, distance_from_prototype),
+            fuzzy.OR(concept_activation, proportion_of_neighbours),
+        )
 
     def engender_follow_up(self, confidence: float) -> Codelet:
         return TopDownClassifierCodelet(
-            self.bubble_chamber,
-            self.parent_concept,
-            None,
-            self.classification_weights,
-            confidence,
+            self.bubble_chamber, self.parent_concept, None, confidence,
         )
--- tests/unit/codelets/test_top_down_classifier_codelet.py
+++ tests/unit/codelets/test_top_down_classifier_codelet.py
@@ -4,23 +4,44 @@ from unittest.mock import Mock
 
 from homer.codelets.top_down_classifier_codelet import TopDownClassifierCodelet
 
-FLOAT_COMPARISON_TOLERANCE = 1e-5
+FLOAT_COMPARISON_TOLERANCE = 1e-1
 
 
 @pytest.mark.parametrize(
-    "activation,depth,distance,proportion_of_neighbours,weights,expected",
-    [(1.0, 1, 0, 1.0, [0.5, -0.1, -0.1, 0.5], 0.9)],
+    "concept_activation, concept_depth, distance_from_prototype, "
+    + "proportion_of_neighbours, expected",
+    [
+        (1.0, 0.1, 0.0, 1.0, 1.0),
+        (0.0, 0.1, 0.0, 1.0, 1.0),
+        (1.0, 1.0, 0.0, 1.0, 1.0),
+        (1.0, 0.1, 1.0, 1.0, 1.0),
+        (1.0, 0.1, 0.0, 0.0, 1.0),
+        (0.5, 0.5, 0.5, 0.5, 0.5),
+        (1.0, 1.0, 1.0, 0.0, 0.0),
+        (0.0, 0.1, 1.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0, 0.0, 0.0),
+        (0.0, 1.0, 1.0, 1.0, 0.0),
+        (0.0, 1.0, 1.0, 0.0, 0.0),
+    ],
 )
 def test_calculate_confidence(
-    activation, depth, distance, proportion_of_neighbours, weights, expected,
+    concept_activation,
+    concept_depth,
+    distance_from_prototype,
+    proportion_of_neighbours,
+    expected,
 ):
-    codelet = TopDownClassifierCodelet(Mock(), Mock(), Mock(), weights, Mock())
-    inputs = [activation, depth, distance, proportion_of_neighbours]
-    result = codelet.calculate_confidence(inputs)
+    codelet = TopDownClassifierCodelet(Mock(), Mock(), Mock(), Mock())
+    result = codelet._calculate_confidence(
+        concept_activation,
+        concept_depth,
+        distance_from_prototype,
+        proportion_of_neighbours,
+    )
     assert math.isclose(expected, result, abs_tol=FLOAT_COMPARISON_TOLERANCE)
 
 
 def test_engender_follow_up():
-    codelet = TopDownClassifierCodelet(Mock(), Mock(), Mock(), Mock(), Mock())
+    codelet = TopDownClassifierCodelet(Mock(), Mock(), Mock(), Mock())
     follow_up = codelet.engender_follow_up(Mock())
     assert TopDownClassifierCodelet == type(follow_up)
