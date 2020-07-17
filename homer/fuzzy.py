186ec5647d3059afc9417e5bb68b847f8accf384
George Wright <george.alexander.wright@gmail.com>
2020-07-17 17:28:57 +0100

    allow n arguments for fuzzy logic gates

--- homer/fuzzy.py
+++ homer/fuzzy.py
@@ -1,26 +1,53 @@
+from functools import reduce
+
+
 def NOT(a: float) -> float:
     return 1.0 - a
 
 
-def AND(a: float, b: float) -> float:
+def AND(*arg):
+    return reduce(_AND, arg)
+
+
+def OR(*arg):
+    return reduce(_OR, arg)
+
+
+def NAND(*arg):
+    return reduce(_NAND, arg)
+
+
+def NOR(*arg):
+    return reduce(_NOR, arg)
+
+
+def XOR(*arg):
+    return reduce(_XOR, arg)
+
+
+def XNOR(*arg):
+    return reduce(_XNOR, arg)
+
+
+def _AND(a: float, b: float) -> float:
     return a * b
 
 
-def OR(a: float, b: float) -> float:
+def _OR(a: float, b: float) -> float:
     return a + b - a * b
 
 
-def NAND(a: float, b: float) -> float:
-    return NOT(AND(a, b))
+def _NAND(a: float, b: float) -> float:
+    return NOT(_AND(a, b))
 
 
-def NOR(a: float, b: float) -> float:
-    return NOT(OR(a, b))
+def _NOR(a: float, b: float) -> float:
+    return NOT(_OR(a, b))
 
 
-def XOR(a: float, b: float) -> float:
-    return AND(OR(a, b), NAND(a, b))
+def _XOR(a: float, b: float) -> float:
+    return AND(_OR(a, b), _NAND(a, b))
 
 
-def XNOR(a: float, b: float) -> float:
-    return NOT(XOR(a, b))
+def _XNOR(a: float, b: float) -> float:
+    return NOT(_XOR(a, b))
--- tests/unit/test_fuzzy.py
+++ tests/unit/test_fuzzy.py
@@ -17,46 +17,85 @@ def test_not(a, expected):
 @pytest.mark.parametrize(
     "a, b, expected", [(1.0, 1.0, 1.0), (0.0, 0.0, 0.0), (0.5, 0.5, 0.25)]
 )
-def test_and(a, b, expected):
-    result = fuzzy.AND(a, b)
+def test_binary_and(a, b, expected):
+    result = fuzzy._AND(a, b)
+    assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
+
+
+@pytest.mark.parametrize(
+    "args, expected",
+    [
+        ([1.0, 1.0, 1.0, 1.0], 1.0),
+        ([0.0, 0.0, 0.0, 0.0], 0.0),
+        ([0.5, 0.5, 0.5, 0.5], 0.0625),
+    ],
+)
+def test_nary_and(args, expected):
+    result = fuzzy.AND(*args)
     assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
 
 
 @pytest.mark.parametrize(
     "a, b, expected", [(1.0, 1.0, 1.0), (0.0, 0.0, 0.0), (0.5, 0.5, 0.75)]
 )
-def test_or(a, b, expected):
-    result = fuzzy.OR(a, b)
+def test_binary_or(a, b, expected):
+    result = fuzzy._OR(a, b)
+    assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
+
+
+@pytest.mark.parametrize(
+    "args, expected",
+    [
+        ([1.0, 1.0, 1.0, 1.0], 1.0),
+        ([0.0, 0.0, 0.0, 0.0], 0.0),
+        ([0.5, 0.5, 0.5, 0.5], 0.9375),
+    ],
+)
+def test_nary_or(args, expected):
+    result = fuzzy.OR(*args)
     assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
 
 
 @pytest.mark.parametrize(
     "a, b, expected", [(1.0, 1.0, 0.0), (0.0, 0.0, 1.0), (0.5, 0.5, 0.75)]
 )
-def test_nand(a, b, expected):
-    result = fuzzy.NAND(a, b)
+def test_binary_nand(a, b, expected):
+    result = fuzzy._NAND(a, b)
     assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
 
 
 @pytest.mark.parametrize(
     "a, b, expected", [(1.0, 1.0, 0.0), (0.0, 0.0, 1.0), (0.5, 0.5, 0.25)]
 )
-def test_nor(a, b, expected):
-    result = fuzzy.NOR(a, b)
+def test_binary_nor(a, b, expected):
+    result = fuzzy._NOR(a, b)
     assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
 
 
 @pytest.mark.parametrize(
     "a, b, expected", [(1.0, 1.0, 0.0), (0.0, 0.0, 0.0), (0.5, 0.5, 0.5625)]
 )
-def test_xor(a, b, expected):
-    result = fuzzy.XOR(a, b)
+def test_binary_xor(a, b, expected):
+    result = fuzzy._XOR(a, b)
+    assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
+
+
+@pytest.mark.parametrize(
+    "args, expected",
+    [
+        ([1.0, 1.0, 1.0, 1.0], 0.0),
+        ([0.0, 0.0, 0.0, 0.0], 0.0),
+        ([0.5, 0.5, 0.5, 0.5], 0.56156),
+    ],
+)
+def test_nary_xor(args, expected):
+    result = fuzzy.XOR(*args)
     assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
 
 
 @pytest.mark.parametrize(
     "a, b, expected", [(1.0, 1.0, 1.0), (0.0, 0.0, 1.0), (0.5, 0.5, 0.4375)]
 )
-def test_xnor(a, b, expected):
-    result = fuzzy.XNOR(a, b)
+def test_binary_xnor(a, b, expected):
+    result = fuzzy._XNOR(a, b)
     assert math.isclose(expected, result, abs_tol=FLOATING_POINT_TOLERANCE)
