c83da86aa12e572ba7ff4b7561b7b31b10b3926a
George Wright <george.alexander.wright@gmail.com>
2020-07-21 16:51:45 +0100

    implement group extender codelet

--- homer/codelets/group_extender_codelet.py
+++ homer/codelets/group_extender_codelet.py
@@ -3,7 +3,6 @@ from typing import Optional
 from homer import fuzzy
 from homer.bubble_chamber import BubbleChamber
 from homer.codelet import Codelet
-from homer.concept import Concept
 from homer.hyper_parameters import HyperParameters
 from homer.perceptlet import Perceptlet
 
@@ -28,7 +27,16 @@ class GroupExtenderCodelet(Codelet):
         return self.engender_follow_up(self.group.strength)
 
     def _calculate_confidence(self, candidate: Perceptlet) -> float:
-        pass
+        common_concepts = {
+            label.parent_concept for label in candidate.labels
+        }.intersection({label.parent_concept for label in self.group.labels})
+        distances = [
+            concept.distance_between_as_rating(candidate.value, self.group.value)
+            for concept in common_concepts
+        ]
+        if distances == []:
+            return 0.0
+        return fuzzy.OR(*distances)
 
-    def engenger_follow_up(self, urgency: float):
+    def engender_follow_up(self, urgency: float):
         return GroupExtenderCodelet(self.bubble_chamber, self.group, urgency)
---
+++ tests/unit/codelets/test_group_extender_codelet.py
@@ -0,0 +1,38 @@
+from unittest.mock import Mock, patch
+
+from homer.codelets.group_extender_codelet import GroupExtenderCodelet
+from homer.concept import Concept
+from homer.perceptlet import Perceptlet
+from homer.perceptlets.group import Group
+from homer.perceptlets.label import Label
+
+
+def test_calculate_confidence():
+    distance = 0.5
+    expected = 0.5
+    with patch.object(Concept, "distance_between_as_rating", return_value=distance):
+        common_concept = Concept(Mock())
+        label_1 = Label(common_concept, Mock())
+        label_2 = Label(common_concept, Mock())
+        group = Group(Mock(), Mock(), Mock(), Mock())
+        group.add_label(label_1)
+        candidate = Perceptlet(Mock(), Mock())
+        candidate.add_label(label_2)
+        codelet = GroupExtenderCodelet(Mock(), group, Mock())
+        confidence = codelet._calculate_confidence(candidate)
+        assert expected == confidence
+
+
+def test_calculate_confidence_with_no_common_concepts():
+    expected = 0.0
+    group = Group(Mock(), Mock(), Mock(), Mock())
+    candidate = Perceptlet(Mock(), Mock())
+    codelet = GroupExtenderCodelet(Mock(), group, Mock())
+    confidence = codelet._calculate_confidence(candidate)
+    assert expected == confidence
+
+
+def test_engender_follow_up():
+    codelet = GroupExtenderCodelet(Mock(), Mock(), Mock())
+    follow_up = codelet.engender_follow_up(Mock())
+    assert GroupExtenderCodelet == type(follow_up)
