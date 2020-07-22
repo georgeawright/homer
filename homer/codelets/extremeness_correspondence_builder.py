1d26a49a765e18f339b0fe20350c9e695edf8231
George Wright <george.alexander.wright@gmail.com>
2020-07-22 17:14:28 +0100

    suggestions for correspondence building codelets

---
+++ homer/codelets/extremeness_correspondence_builder.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from homer.bubble_chamber import BubbleChamber
+from homer.codelet import Codelet
+from homer.hyper_parameters import HyperParameters
+from homer.perceptlets.group import Group
+
+
+class ExtremenessCorrespondenceBuilder(Codelet):
+
+    CONFIDENCE_THRESHOLD = HyperParameters.CONFIDENCE_THRESHOLD
+
+    def __init__(
+        self,
+        bubble_chamber: BubbleChamber,
+        target_group_a: Group,
+        target_group_b: Group,
+    ):
+        self.bubble_chamber = bubble_chamber
+        self.target_group_a = target_group_a
+        self.target_group_b = target_group_b
+
+    def run(self) -> Optional[Codelet]:
+        pass
+
+    def _calculate_confidence(self) -> float:
+        """Returns a high value for groups where one is more extreme."""
+        pass
+
+    def _engender_follow_up(self, urgency: float) -> Codelet:
+        pass
