92209329c9fb0cd96f0c14838cb9eb0e6d89f760
George Wright <george.alexander.wright@gmail.com>
2020-07-30 17:52:27 +0100

    add bottom up raw perceptlet labeler

---
+++ homer/codelets/bottom_up_raw_perceptlet_labeler.py
@@ -0,0 +1,50 @@
+from homer.bubble_chamber import BubbleChamber
+from homer.codelet import Codelet
+from homer.codelets.raw_perceptlet_labeler import RawPerceptletLabeler
+from homer.concept import Concept
+from homer.hyper_parameters import HyperParameters
+from homer.perceptlet import Perceptlet
+
+
+class BottomUpRawPerceptletLabeler(Codelet):
+
+    CONFIDENCE_THRESHOLD = HyperParameters.CONFIDENCE_THRESHOLD
+
+    def __init__(
+        self,
+        bubble_chamber: BubbleChamber,
+        target_perceptlet: Perceptlet,
+        urgency: float,
+    ):
+        self.bubble_chamber = bubble_chamber
+        self.target_perceptlet = target_perceptlet
+        self.urgency = urgency
+
+    def run(self):
+        concept = self.bubble_chamber.get_random_workspace_concept()
+        confidence_of_class_membership = concept.proximity_to(self.target)
+        if confidence_of_class_membership > self.CONFIDENCE_THRESHOLD:
+            concept.boost_activation(
+                confidence_of_class_membership, self.target_perceptlet.location
+            )
+            label = self.bubble_chamber.create_label(
+                concept,
+                self.target_perceptlet.location,
+                self.target_perceptlet.time,
+                self.target_perceptlet.strength,
+            )
+            self.target_perceptlet.add_label(label)
+            return self._engender_follow_up(concept, confidence_of_class_membership)
+        return self._engenger_alternative_follow_up(confidence_of_class_membership)
+
+    def _engender_follow_up(self, concept: Concept, urgency: float) -> Codelet:
+        new_target_perceptlet = self.target_perceptlet.get_random_neighbour
+        return RawPerceptletLabeler(
+            self.bubble_chamber, concept, new_target_perceptlet, urgency
+        )
+
+    def _engender_alternative_follow_up(self, urgency: float) -> Codelet:
+        new_target_perceptlet = self.bubble_chamber.get_raw_perceptlet()
+        return BottomUpRawPerceptletLabeler(
+            self.bubble_chamber, new_target_perceptlet, urgency
+        )
---
+++ tests/unit/perceptlets/test_bottom_up_raw_perceptlet_labeler.py
@@ -0,0 +1,22 @@
+from unittest.mock import Mock
+
+from homer.codelets.bottom_up_raw_perceptlet_labeler import BottomUpRawPerceptletLabeler
+from homer.codelets.raw_perceptlet_labeler import RawPerceptletLabeler
+
+
+def test_engender_follow_up():
+    bottom_up_raw_perceptlet_labeler = BottomUpRawPerceptletLabeler(
+        Mock(), Mock(), Mock()
+    )
+    follow_up = bottom_up_raw_perceptlet_labeler._engender_follow_up(Mock(), Mock())
+    assert RawPerceptletLabeler == type(follow_up)
+
+
+def test_engender_alternative_follow_up():
+    bottom_up_raw_perceptlet_labeler = BottomUpRawPerceptletLabeler(
+        Mock(), Mock(), Mock()
+    )
+    alternative_follow_up = bottom_up_raw_perceptlet_labeler._engender_alternative_follow_up(
+        Mock()
+    )
+    assert BottomUpRawPerceptletLabeler == type(alternative_follow_up)
