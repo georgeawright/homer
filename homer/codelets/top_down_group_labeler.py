260f065870d85bb78fc5fa133f608b09421f4480
George Wright <george.alexander.wright@gmail.com>
2020-08-13 16:59:48 +0100

    implement top down group labeler

---
+++ homer/codelets/top_down_group_labeler.py
@@ -0,0 +1,56 @@
+from homer.bubble_chamber import BubbleChamber
+from homer.codelet import Codelet
+from homer.codelets.group_extender import GroupExtender
+from homer.concept import Concept
+from homer.concepts.perceptlet_type import PerceptletType
+from homer.perceptlets.group import Group
+
+
+class TopDownGroupLabeler(Codelet):
+    def __init__(
+        self,
+        bubble_chamber: BubbleChamber,
+        perceptlet_type: PerceptletType,
+        parent_concept: Concept,
+        target_group: Group,
+        urgency: float,
+        parent_id: str,
+    ):
+        Codelet.__init__(self, bubble_chamber, parent_id)
+        self.perceptlet_type = perceptlet_type
+        self.parent_concept = parent_concept
+        self.target_perceptlet = target_group
+        self.urgency = urgency
+
+    def _passes_preliminary_checks(self) -> bool:
+        return not self.target_perceptlet.has_label(self.parent_concept)
+
+    def _fizzle(self):
+        self.perceptlet_type.decay_activation(self.target_perceptlet.location)
+        return None
+
+    def _calculate_confidence(self):
+        total_strength = 0.0
+        for member in self.target_perceptlet.members:
+            for label in member.labels:
+                if label.parent_concept == self.parent_concept:
+                    total_strength += label.strength
+        self.confidence = total_strength / self.target_perceptlet.size
+
+    def _process_perceptlet(self):
+        label = self.bubble_chamber.create_label(
+            self.parent_concept,
+            self.target_perceptlet.location,
+            self.confidence,
+            self.codelet_id,
+        )
+        self.target_perceptlet.add_label(label)
+
+    def _engender_follow_up(self) -> GroupExtender:
+        return GroupExtender(
+            self.bubble_chamber,
+            self.bubble_chamber.concept_space.get_perceptlet_type_by_name("group"),
+            self.target_perceptlet,
+            self.confidence,
+            self.codelet_id,
+        )
---
+++ tests/unit/codelets/test_top_down_group_labeler.py
@@ -0,0 +1,38 @@
+import math
+import pytest
+from unittest.mock import Mock
+
+from homer.codelets.top_down_group_labeler import TopDownGroupLabeler
+from homer.codelets.group_extender import GroupExtender
+
+FLOAT_COMPARISON_TOLERANCE = 1e-1
+
+
+@pytest.mark.parametrize(
+    "label_strengths, group_size, expected",
+    [([0.5, 0.5], 2, 0.5), ([0.5, 0.5], 4, 0.25)],
+)
+def test_calculate_confidence(label_strengths, group_size, expected):
+    concept = Mock()
+    group = Mock()
+    group.size = group_size
+    group.members = set()
+    for label_strength in label_strengths:
+        label = Mock()
+        label.parent_concept = concept
+        label.strength = label_strength
+        member = Mock()
+        member.labels = {label}
+        group.members.add(member)
+    codelet = TopDownGroupLabeler(Mock(), Mock(), concept, group, Mock(), Mock())
+    codelet._calculate_confidence()
+    assert math.isclose(
+        expected, codelet.confidence, abs_tol=FLOAT_COMPARISON_TOLERANCE
+    )
+
+
+def test_engender_follow_up():
+    codelet = TopDownGroupLabeler(Mock(), Mock(), Mock(), Mock(), Mock(), Mock())
+    codelet.confidence = Mock()
+    follow_up = codelet._engender_follow_up()
+    assert GroupExtender == type(follow_up)
