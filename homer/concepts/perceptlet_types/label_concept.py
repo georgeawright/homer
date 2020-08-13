9a891a9e5173d3b09a800c9b335214475150545c
George Wright <george.alexander.wright@gmail.com>
2020-08-13 17:16:07 +0100

    type spawn codelet

--- homer/concepts/perceptlet_types/label_concept.py
+++ homer/concepts/perceptlet_types/label_concept.py
@@ -1,4 +1,5 @@
 import random
+from typing import Optional
 
 from homer.bubble_chamber import BubbleChamber
 from homer.codelets.bottom_up_raw_perceptlet_labeler import BottomUpRawPerceptletLabeler
@@ -9,7 +10,9 @@ class LabelConcept(PerceptletType):
     def __init__(self, name: str = "label"):
         PerceptletType.__init__(self, name)
 
-    def spawn_codelet(self, bubble_chamber: BubbleChamber):
+    def spawn_codelet(
+        self, bubble_chamber: BubbleChamber
+    ) -> Optional[BottomUpRawPerceptletLabeler]:
         if self.activation_pattern.is_high():
             target_perceptlet = bubble_chamber.get_unhappy_raw_perceptlet()
             return BottomUpRawPerceptletLabeler(
