8bf6fc6e5f67129d2655bfeb3e4007bda3796ca3
George Wright <george.alexander.wright@gmail.com>
2020-08-14 16:31:21 +0100

    implement add, remove, union, intersection for perceptlet collection

--- homer/perceptlet_collection.py
+++ homer/perceptlet_collection.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import random
-from typing import Iterable
+from typing import List, Set, Union
 
 from .errors import MissingPerceptletError
 from .perceptlet import Perceptlet
@@ -8,7 +8,7 @@ from .workspace_location import WorkspaceLocation
 
 
 class PerceptletCollection:
-    def __init__(self, perceptlets: Iterable[Perceptlet]):
+    def __init__(self, perceptlets: Set[Perceptlet]):
         self.perceptlets = perceptlets
         self.perceptlets_by_location = None
 
@@ -17,6 +17,38 @@ class PerceptletCollection:
             self._arrange_perceptlets_by_location()
         return PerceptletCollection(
             self.perceptlets_by_location[location.i][location.j][location.k]
+        )
+
+    def add(self, perceptlet: Perceptlet):
+        self.perceptlets.add(perceptlet)
+        if self.perceptlets_by_location is not None:
+            self._add_at_location(perceptlet, perceptlet.location)
+            try:
+                for member in perceptlet.member:
+                    self._add_at_location(perceptlet, member.location)
+            except AttributeError:
+                pass
+
+    def remove(self, perceptlet: Perceptlet):
+        self.perceptlets.remove(perceptlet)
+        if self.perceptlets_by_location is None:
+            return
+        for i, layer in enumerate(self.perceptlets_by_location):
+            for j, row in enumerate(layer):
+                for k, cell in enumerate(layer):
+                    if perceptlet in cell:
+                        self.perceptlets_by_location[i][j][k].remove(perceptlet)
+
+    @staticmethod
+    def union(*collections: List[PerceptletCollection]) -> PerceptletCollection:
+        return PerceptletCollection(
+            set.union(*[collection.perceptlets for collection in collections])
+        )
+
+    @staticmethod
+    def intersection(*collections: List[PerceptletCollection]) -> PerceptletCollection:
+        return PerceptletCollection(
+            set.intersection(*[collection.perceptlets for collection in collections])
         )
 
     def get_random(self) -> Perceptlet:
@@ -34,6 +66,13 @@ class PerceptletCollection:
     def get_unhappy(self) -> Perceptlet:
         """Returns a perceptlet probabilistically according to unhappiness."""
         return self._get_perceptlet_according_to("unhappiness")
+
+    def _add_at_location(
+        self, perceptlet: Perceptlet, coordinates: List[Union[float, int]]
+    ):
+
+        loc = WorkspaceLocation.from_workspace_coordinates(coordinates)
+        self.perceptlets_by_location[loc.i][loc.j][loc.k]
 
     def _get_perceptlet_according_to(self, attribute: str) -> Perceptlet:
         """Returns a perceptlet probabilistically according to attribute."""
--- tests/unit/test_perceptlet_collection.py
+++ tests/unit/test_perceptlet_collection.py
@@ -3,6 +3,41 @@ from unittest.mock import Mock
 from homer.perceptlet_collection import PerceptletCollection
 
 
+def test_add():
+    collection = PerceptletCollection(set())
+    assert collection.perceptlets == set()
+    perceptlet = Mock()
+    collection.add(perceptlet)
+    assert collection.perceptlets == {perceptlet}
+
+
+def test_remove():
+    perceptlet = Mock()
+    collection = PerceptletCollection({perceptlet})
+    assert collection.perceptlets == {perceptlet}
+    collection.remove(perceptlet)
+    assert collection.perceptlets == set()
+
+
+def test_union():
+    perceptlet_1 = Mock()
+    perceptlet_2 = Mock()
+    collection_1 = PerceptletCollection({perceptlet_1})
+    collection_2 = PerceptletCollection({perceptlet_2})
+    union = PerceptletCollection.union(collection_1, collection_2)
+    assert union.perceptlets == {perceptlet_1, perceptlet_2}
+
+
+def test_intersection():
+    perceptlet_1 = Mock()
+    perceptlet_2 = Mock()
+    perceptlet_3 = Mock()
+    collection_1 = PerceptletCollection({perceptlet_1, perceptlet_2})
+    collection_2 = PerceptletCollection({perceptlet_2, perceptlet_3})
+    intersection = PerceptletCollection.intersection(collection_1, collection_2)
+    assert intersection.perceptlets == {perceptlet_2}
+
+
 def test_get_random():
     perceptlets = {Mock() for _ in range(10)}
     collection = PerceptletCollection(perceptlets)
