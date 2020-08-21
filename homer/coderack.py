82afbb1578281b8cc8634bb97ebce9b1b38fc829
George Wright <george.alexander.wright@gmail.com>
2020-08-21 10:55:54 +0100

    force stop at 1000

--- homer/coderack.py
+++ homer/coderack.py
@@ -40,6 +40,8 @@ class Coderack:
                 codelet_choice = codelet
         if codelet_choice is None:
             raise NoMoreCodelets
+        if self.codelets_run > 1000:
+            raise NoMoreCodelets
         self._codelets.remove(codelet_choice)
         return codelet_choice
 
