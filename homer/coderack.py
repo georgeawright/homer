2d328a8514053652b91b84ac5356b530de497012
George Wright <george.alexander.wright@gmail.com>
2020-08-27 14:17:34 +0100

    set codelets max to 10,000

--- homer/coderack.py
+++ homer/coderack.py
@@ -43,7 +43,7 @@ class Coderack:
                 codelet_choice = codelet
         if codelet_choice is None:
             raise NoMoreCodelets
-        if self.codelets_run > 5000:
+        if self.codelets_run > 10000:
             raise NoMoreCodelets
         self._codelets.remove(codelet_choice)
         return codelet_choice
