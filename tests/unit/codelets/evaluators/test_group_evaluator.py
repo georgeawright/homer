ba893f344ecb295e5107b4feead7e5b4d07e82e8
George Wright <george.alexander.wright@gmail.com>
2020-09-01 10:46:40 +0100

    test group evaluator engender follow up

--- tests/unit/codelets/evaluators/test_group_evaluator.py
+++ tests/unit/codelets/evaluators/test_group_evaluator.py
@@ -3,6 +3,7 @@ import pytest
 from unittest.mock import Mock
 
 from homer.codelets.evaluators import GroupEvaluator
+from homer.codelets.selectors import GroupSelector
 
 
 FLOATING_POINT_TOLERANCE = 1e-1
@@ -34,3 +35,18 @@ def test_calculate_confidence(group_value, member_values, expected):
     assert math.isclose(
         expected, evaluator.confidence, abs_tol=FLOATING_POINT_TOLERANCE
     )
+
+
+def test_engender_follow_up():
+    target_group = Mock()
+    target_group.location = [0, 0, 0]
+    bubble_chamber = Mock()
+    bubble_chamber.concept_space = {"group-selection": Mock()}
+    collection = Mock()
+    collection.get_most_active.side_effect = [target_group]
+    bubble_chamber.workspace.groups.at.side_effect = [collection]
+    evaluator = GroupEvaluator(
+        bubble_chamber, Mock(), Mock(), target_group, Mock(), Mock()
+    )
+    follow_up = evaluator._engender_follow_up()
+    assert GroupSelector == type(follow_up)
