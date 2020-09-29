9e31a6984da69fbafd2447ddf9c909506fc7174c
George Wright <george.alexander.wright@gmail.com>
2020-09-29 15:54:15 +0100

    decay perceptlet activation by multiplication

--- homer/activation_patterns/perceptlet_activation_pattern.py
+++ homer/activation_patterns/perceptlet_activation_pattern.py
@@ -16,4 +16,4 @@ class PerceptletActivationPattern(ScalarActivationPattern):
         self.activation = min(max(raw_activation, 0.0), 1.0)
 
     def decay_by_amount(self, amount: float):
-        self.boost_by_amount(-amount)
+        self.activation *= amount
