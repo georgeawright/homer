f345bbfe0ece9102a9b217436054871653420695
George Wright <george.alexander.wright@gmail.com>
2020-10-05 12:43:58 +0100

    implement float between one and zero type inheriting from float

---
+++ tests/unit/test_float_between_one_and_zero.py
@@ -0,0 +1,11 @@
+import pytest
+
+from homer.float_between_one_and_zero import FloatBetweenOneAndZero
+
+
+@pytest.mark.parametrize(
+    "raw_float, equivalent_float_value",
+    [(0.5, 0.5), (0.0, 0.0), (1.0, 1.0), (-0.1, 0.0), (1.1, 1.0)],
+)
+def test_constructor(raw_float, equivalent_float_value):
+    assert FloatBetweenOneAndZero(raw_float) == equivalent_float_value
