c001b1e37e0c805dab4741572820831627ae9446
George Wright <george.alexander.wright@gmail.com>
2020-10-05 10:41:59 +0100

    implement evaluator outline

--- homer/codelets/evaluator.py
+++ homer/codelets/evaluator.py
@@ -1,48 +1,80 @@
-from homer.bubble_chamber import BubbleChamber
+from __future__ import annotations
+
 from homer.bubbles import Perceptlet
-from homer.bubbles.concepts.perceptlet_type import PerceptletType
+from homer.classifier import Classifier
 from homer.codelet import Codelet
-from homer.hyper_parameters import HyperParameters
+from homer.strategy import Strategy
+
+from .builder import Builder
 
 
 class Evaluator(Codelet):
-
-    CONFIDENCE_THRESHOLD = HyperParameters.EVALUATOR_CONFIDENCE_THRESHOLD
-
     def __init__(
         self,
-        bubble_chamber: BubbleChamber,
-        perceptlet_type: PerceptletType,
-        target_type: PerceptletType,
-        target_perceptlet: Perceptlet,
         urgency: float,
+        codelet_id: str,
         parent_id: str,
+        target_perceptlet: Perceptlet,
+        child_perceptlet_type: type,
+        classifier: Classifier,
     ):
-        parent_concept = None
-        Codelet.__init__(
-            self,
-            bubble_chamber,
-            perceptlet_type,
-            parent_concept,
-            target_perceptlet,
+        Codelet.__init__(self, urgency, codelet_id, parent_id)
+        self.target_perceptlet = target_perceptlet
+        self.child_perceptlet_type = child_perceptlet_type
+        self.classifier = classifier
+
+    @classmethod
+    def from_components(
+        cls,
+        urgency: float = None,
+        parent_id: str = None,
+        strategy: Strategy = None,
+        target_perceptlet_type: Perceptlet = None,
+        child_perceptlet_type: type = None,
+    ) -> Builder:
+        codelet_id = None
+        target_perceptlet = target_perceptlet_type.instances.get_exigent()
+        urgency = urgency if urgency is not None else target_perceptlet.exigency()
+        return Builder(
             urgency,
+            codelet_id,
             parent_id,
+            target_perceptlet,
+            child_perceptlet_type,
+            target_perceptlet_type.classifier,
         )
-        self.target_type = target_type
 
-    def _fizzle(self):
-        self.perceptlet_type.activation.decay(self.location)
-        return None
+    def run(self):
+        if self._passes_checks():
+            confidence = self.classifier.confidence()
+            self.target_perceptlet.quality = confidence
+            self.child_codelets.append()
+        else:
+            self.child_codelets.append(
+                Builder.from_components(
+                    parent_id=self.codelet_id,
+                    target_perceptlet_type=type(self.target_perceptlet),
+                    child_perceptlet_type=self.child_perceptlet_type,
+                )
+            )
 
-    def _fail(self):
-        pass
-
-    def _process_perceptlet(self):
-        self.target_perceptlet.quality += self.confidence
-        if self.confidence > 0:
-            self.target_type.activation.decay(self.location)
-        if self.confidence < 0:
-            self.target_type.activation.boost(abs(self.confidence), self.location)
-        self.bubble_chamber.logger.log_perceptlet_update(
-            self, self.target_perceptlet, "Quality updated"
+    def _reproduce(
+        self,
+        urgency: float = None,
+        child_perceptlet_type: type = None,
+        strategy: Strategy = None,
+    ) -> Builder:
+        if urgency is None:
+            urgency = self.urgency / 2
+        if child_perceptlet_type is None:
+            child_perceptlet_type = self.child_perceptlet_type
+        if strategy is None:
+            strategy = self.strategy
+        target_perceptlet_type = type(self.target_perceptlet)
+        return Builder.from_components(
+            urgency,
+            self.codelet_id,
+            strategy,
+            target_perceptlet_type,
+            child_perceptlet_type,
         )
