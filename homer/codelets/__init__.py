6686e71e36a0976f2269b11ce7c8abb52ecd21f1
George Wright <george.alexander.wright@gmail.com>
2020-10-29 10:44:50 +0000

    remove old codelets

--- homer/codelets/__init__.py
+++ homer/codelets/__init__.py
@@ -1,16 +1,3 @@
-from .bottom_up_raw_perceptlet_labeler import BottomUpRawPerceptletLabeler
-from .raw_perceptlet_labeler import RawPerceptletLabeler
-
-from .group_builder import GroupBuilder
-from .group_extender import GroupExtender
-from .group_labeler import GroupLabeler
-from .top_down_group_labeler import TopDownGroupLabeler
-
-from .correspondence_suggester import CorrespondenceSuggester
-from .correspondence_builder import CorrespondenceBuilder
-from .correspondence_labeler import CorrespondenceLabeler
-
-from .textlet_builder import TextletBuilder
-
+from .builder import Builder
 from .evaluator import Evaluator
 from .selector import Selector
