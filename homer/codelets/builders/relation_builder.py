f831da8d62fe7846669e05e08a882ee929b7ee19
George Wright <george.alexander.wright@gmail.com>
2020-10-30 12:27:38 +0000

    implement and test relation builder

--- homer/codelets/builders/relation_builder.py
+++ homer/codelets/builders/relation_builder.py
@@ -1,27 +1,113 @@
+from homer.bubble_chamber import BubbleChamber
 from homer.codelets.builder import Builder
+from homer.float_between_one_and_zero import FloatBetweenOneAndZero
+from homer.structure import Structure
+from homer.structures import Concept
+from homer.structures.links import Relation
+
+from .chunk_builder import ChunkBuilder
 
 
 class RelationBuilder(Builder):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        codelet_id: str,
+        parent_id: str,
+        structure_concept: Concept,
+        bubble_chamber: BubbleChamber,
+        target_structure_one: Structure,
+        urgency: FloatBetweenOneAndZero,
+        target_structure_two: Structure = None,
+        parent_concept: Concept = None,
+    ):
+        Builder.__init__(self, codelet_id, parent_id, urgency)
+        self.structure_concept = structure_concept
+        self.bubble_chamber = bubble_chamber
+        self.target_structure_one = target_structure_one
+        self.target_structure_two = target_structure_two
+        self.parent_concept = parent_concept
+        self.confidence = 0.0
+        self.child_structure = None
+
+    @classmethod
+    def spawn(
+        cls,
+        parent_id: str,
+        bubble_chamber: BubbleChamber,
+        target_structure_one: Structure,
+        urgency: FloatBetweenOneAndZero,
+        target_structure_two: Structure = None,
+        parent_concept: Concept = None,
+    ):
+        codelet_id = ""
+        structure_concept = bubble_chamber.concepts["relation"]
+        return cls(
+            codelet_id,
+            parent_id,
+            structure_concept,
+            bubble_chamber,
+            target_structure_one,
+            urgency,
+            target_structure_two,
+            parent_concept,
+        )
 
     def _passes_preliminary_checks(self):
-        pass
+        if self.target_structure_two is None:
+            # parent concept and space need to be compatible
+            self.target_structure_two = (
+                self.target_structure_one.parent_spaces.get_random().contents.get_exigent()
+            )
+        if self.parent_concept is None:
+            # parent concept and space need to be compatible
+            self.parent_concept = self.bubble_chamber.get_random_relation_concept()
+        return not self.target_structure_one.has_relation(
+            self.parent_concept, self.target_structure_two
+        )
 
     def _calculate_confidence(self):
-        pass
+        self.confidence = self.parent_concept.classify(
+            self.target_structure_one, self.target_structure_two
+        )
 
     def _boost_activations(self):
         pass
 
-    def _process_perceptlet(self):
-        pass
+    def _process_structure(self):
+        relation = Relation(
+            self.target_structure_one, self.target_structure_two, self.parent_concept
+        )
+        self.target_structure_one.add_relation(relation)
+        self.target_structure_two.add_relation(relation)
+        self.bubble_chamber.add_relation(relation)
+        self.child_structure = relation
 
     def _engender_follow_up(self):
-        pass
+        new_target = self.target_structure_one.nearby.get_unhappy()
+        self.child_codelets.append(
+            RelationBuilder.spawn(
+                self.codelet_id,
+                self.bubble_chamber,
+                new_target,
+                self.confidence,
+                self.parent_concept,
+            )
+        )
 
     def _fizzle(self):
-        pass
+        new_target = self.target_structure_one.parent_space.contents.get_random()
+        self.child_codelets.append(
+            ChunkBuilder.spawn(
+                self.codelet_id, self.bubble_chamber, new_target, new_target.unhappiness
+            )
+        )
 
     def _fail(self):
-        pass
+        self.child_codelets.append(
+            RelationBuilder.spawn(
+                self.codelet_id,
+                self.bubble_chamber,
+                self.target_structure_one,
+                self.target_structure_one.unhappiness,
+            )
+        )
--- tests/unit/codelets/builders/test_relation_builder.py
+++ tests/unit/codelets/builders/test_relation_builder.py
@@ -1,16 +1,66 @@
 import pytest
 from unittest.mock import Mock
 
-from homer.codelets.builders import RelationBuilder
+from homer.codelets.builders import ChunkBuilder, RelationBuilder
 from homer.structures.links import Relation
 
 
-@pytest.mark.skip
-def test_successful_creates_chunk_and_spawns_follow_up():
-    bubble_chamber = Mock()
-    target_structure = Mock()
+@pytest.fixture
+def parent_concept():
+    concept = Mock()
+    concept.classify.return_value = 1.0
+    return concept
+
+
+@pytest.fixture
+def bubble_chamber(parent_concept):
+    chamber = Mock()
+    chamber.concepts = {"chunk": Mock(), "relation": Mock()}
+    chamber.get_random_relation_concept.return_value = parent_concept
+    return chamber
+
+
+@pytest.fixture
+def target_structure_two():
+    structure = Mock()
+    return structure
+
+
+@pytest.fixture
+def target_structure_one(target_structure_two):
+    structure = Mock()
+    parent_space_contents = Mock()
+    parent_space_contents.get_exigent.return_value = target_structure_two
+    structure.parent_spaces.get_random.return_value = parent_space_contents
+    structure.has_relation.return_value = False
+    structure.nearby.get_unhappy.return_value = Mock()
+    return structure
+
+
+def test_bottom_up_codelet_gets_a_concept(bubble_chamber):
+    target_structure_one = Mock()
     relation_builder = RelationBuilder(
-        Mock(), Mock(), Mock(), bubble_chamber, target_structure, Mock()
+        Mock(), Mock(), Mock(), bubble_chamber, target_structure_one, Mock()
+    )
+    assert relation_builder.parent_concept is None
+    relation_builder.run()
+    assert relation_builder.parent_concept is not None
+
+
+def test_codelet_gets_a_second_target_structure(bubble_chamber, target_structure_one):
+    relation_builder = RelationBuilder(
+        Mock(), Mock(), Mock(), bubble_chamber, target_structure_one, Mock()
+    )
+    assert relation_builder.target_structure_two is None
+    relation_builder.run()
+    assert relation_builder.target_structure_two is not None
+
+
+def test_successful_creates_chunk_and_spawns_follow_up(
+    bubble_chamber, target_structure_one
+):
+    relation_builder = RelationBuilder(
+        Mock(), Mock(), Mock(), bubble_chamber, target_structure_one, Mock()
     )
     relation_builder.run()
     assert isinstance(relation_builder.child_structure, Relation)
@@ -18,28 +68,25 @@ def test_successful_creates_chunk_and_spawns_follow_up():
     assert isinstance(relation_builder.child_codelets[0], RelationBuilder)
 
 
-@pytest.mark.skip
-def test_fails_when_structures_cannot_be_related():
-    bubble_chamber = Mock()
-    target_structure = Mock()
+def test_fails_when_structures_cannot_be_related(
+    parent_concept, bubble_chamber, target_structure_one
+):
+    parent_concept.classify.return_value = 0.0
     relation_builder = RelationBuilder(
-        Mock(), Mock(), Mock(), bubble_chamber, target_structure, Mock()
+        Mock(), Mock(), Mock(), bubble_chamber, target_structure_one, Mock()
     )
     relation_builder.run()
     assert relation_builder.child_structure is None
     assert len(relation_builder.child_codelets) == 1
-    # assert isinstance(relation_builder.child_codelets[0], RelationBuilder)
+    assert isinstance(relation_builder.child_codelets[0], RelationBuilder)
 
 
-@pytest.mark.skip
-def test_fizzles_in_unsuitable_conditions():
-    bubble_chamber = Mock()
-    target_structure = Mock()
+def test_fizzles_when_relation_already_exists(bubble_chamber, target_structure_one):
+    target_structure_one.has_relation.return_value = True
     relation_builder = RelationBuilder(
-        Mock(), Mock(), Mock(), bubble_chamber, target_structure, Mock()
+        Mock(), Mock(), Mock(), bubble_chamber, target_structure_one, Mock()
     )
     relation_builder.run()
     assert relation_builder.child_structure is None
     assert len(relation_builder.child_codelets) == 1
-    # should probably spawn chunk builder, ideally builder of the thing it was looking for?
-    # assert isinstance(relation_builder.child_codelets[0], RelationBuilder)
+    assert isinstance(relation_builder.child_codelets[0], ChunkBuilder)
