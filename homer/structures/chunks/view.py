dc911c8eba4311cd2835dc8ee04cf63a36d9df00
George Wright <george.alexander.wright@gmail.com>
2020-10-30 16:55:00 +0000

    add view class (chunk of correspondences)

---
+++ homer/structures/chunks/view.py
@@ -0,0 +1,22 @@
+from homer.structures.chunk import Chunk
+from homer.structures.links.correspondence import Correspondence
+
+
+class View(Chunk):
+    """A chunk of self-consistent correspondences."""
+
+    def __init__(self, members, parent_space):
+        value = None
+        location = None
+        neighbours = None
+        Chunk.__init__(self, value, location, members, neighbours, parent_space)
+
+    @property
+    def size(self):
+        return len(self.members)
+
+    def nearby(self):
+        raise NotImplementedError
+
+    def add_member(self, new_member: Correspondence):
+        pass
