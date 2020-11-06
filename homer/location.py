4127f53a43fd3cd98457d71891ce2fcf131bfba1
George Wright <george.alexander.wright@gmail.com>
2020-11-06 12:25:26 +0000

    implement location with average class method

--- homer/location.py
+++ homer/location.py
@@ -1,2 +1,16 @@
+from __future__ import annotations
+import statistics
+from typing import List
+
+
 class Location:
-    pass
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    @classmethod
+    def average(cls, locations: List[Location]) -> Location:
+        return Location(
+            statistics.fmean([location.x for location in locations]),
+            statistics.fmean([location.y for location in locations]),
+        )
---
+++ tests/unit/test_location.py
@@ -0,0 +1,14 @@
+import pytest
+
+from homer.location import Location
+
+
+@pytest.mark.parametrize(
+    "xs, ys, expected_x, expected_y",
+    [([1, 2, 3], [4, 5, 6], 2, 5), ([1, 2, 3, 4], [4, 5, 6, 7], 2.5, 5.5)],
+)
+def test_average(xs, ys, expected_x, expected_y):
+    locations = [Location(xs[i], ys[i]) for i in range(len(xs))]
+    average = Location.average(locations)
+    assert expected_x == average.x
+    assert expected_y == average.y
