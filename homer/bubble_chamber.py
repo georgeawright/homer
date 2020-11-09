fc447329bc3913f137d534750dcf75bec11066fc
George Wright <george.alexander.wright@gmail.com>
2020-11-09 17:00:52 +0000

    add different structure collections to bubble chamber

--- homer/bubble_chamber.py
+++ homer/bubble_chamber.py
@@ -4,6 +4,7 @@ from typing import List, Union
 
 from .logger import Logger
 from .problem import Problem
+from .structure_collection import StructureCollection
 from .structures import Space
 
 
@@ -12,10 +13,26 @@ class BubbleChamber:
         self,
         top_level_working_space: Space,
         top_level_conceptual_space: Space,
+        chunks: StructureCollection,
+        concepts: StructureCollection,
+        correspondences: StructureCollection,
+        labels: StructureCollection,
+        relations: StructureCollection,
+        spaces: StructureCollection,
+        views: StructureCollection,
+        words: StructureCollection,
         logger: Logger,
     ):
         self.top_level_working_space = top_level_working_space
         self.top_level_conceptual_space = top_level_conceptual_space
+        self.chunks = chunks
+        self.concepts = concepts
+        self.correspondences = correspondences
+        self.labels = labels
+        self.relations = relations
+        self.spaces = spaces
+        self.views = views
+        self.words = words
         self.logger = logger
         self.result = None
 
