9b9c0326fe0e7edc41e6749d7b7f8a9b237f0272
George Wright <george.alexander.wright@gmail.com>
2020-11-23 11:11:30 +0000

    template has contents not members

--- homer/structures/spaces/frames/template.py
+++ homer/structures/spaces/frames/template.py
@@ -10,7 +10,7 @@ class Template(Frame):
     def __init__(
         self,
         name: str,
-        members: StructureCollection,
+        contents: StructureCollection,
         parent_concept: Concept,
         parent_spaces: StructureCollection = None,
         child_spaces: StructureCollection = None,
@@ -20,7 +20,7 @@ class Template(Frame):
         Frame.__init__(
             self,
             name,
-            members,
+            contents,
             parent_concept,
             parent_spaces=parent_spaces,
             child_spaces=child_spaces,
