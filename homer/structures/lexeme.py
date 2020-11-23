1b5ef11ee09a5c8dcc8328c8a70562990209692e
George Wright <george.alexander.wright@gmail.com>
2020-11-23 10:32:04 +0000

    lexeme new method

--- homer/structures/lexeme.py
+++ homer/structures/lexeme.py
@@ -21,5 +21,17 @@ class Lexeme(Structure):
         self.headword = headword
         self.forms = forms
 
+    @classmethod
+    def new(cls, headword: str, forms: Dict[WordForm, str], parent_concept):
+        from homer.structures import Concept
+        from homer.structures.links import Relation
+
+        lexeme = cls(headword, forms)
+        link_to_concept = Relation(parent_concept, lexeme, None, None, 0.0)
+        link_to_concept._activation = 1.0
+        parent_concept.links_out.add(link_to_concept)
+        lexeme.links_in.add(link_to_concept)
+        return lexeme
+
     def get_form(self, form: WordForm) -> str:
         return self.forms[form]
