cdbbbb6e5b8c870f94db27b79ca6ec64cffe327d
George Wright <george.alexander.wright@gmail.com>
2020-11-23 16:38:06 +0000

    add evaluator and selector spawning and run recursively until something is spawned

--- homer/codelets/factory_codelet.py
+++ homer/codelets/factory_codelet.py
@@ -9,6 +9,21 @@ from homer.codelets.builders import (
     ViewBuilder,
     WordBuilder,
 )
+from homer.codelets.evaluators import (
+    ChunkEvaluator,
+    CorrespondenceEvaluator,
+    LabelEvaluator,
+    RelationEvaluator,
+    ViewEvaluator,
+)
+from homer.codelets.selectors import (
+    ChunkSelector,
+    CorrespondenceSelector,
+    LabelSelector,
+    RelationSelector,
+    ViewSelector,
+)
+from homer.errors import MissingStructureError
 from homer.float_between_one_and_zero import FloatBetweenOneAndZero
 from homer.id import ID
 from homer.structure_collection import StructureCollection
@@ -39,9 +54,10 @@ class FactoryCodelet(Codelet):
         return cls(codelet_id, parent_id, bubble_chamber, urgency)
 
     def run(self) -> CodeletResult:
-        action_type = self.bubble_chamber.spaces["activities"].contents.get_active()
-        structure_type = self.bubble_chamber.spaces["structures"].contents.get_active()
-        self._engender_follow_up(action_type, structure_type)
+        try:
+            self._engender_follow_up()
+        except MissingStructureError:
+            self.run()
         self.child_codelets.append(
             self.spawn(
                 self.codelet_id, self.bubble_chamber, self.bubble_chamber.satisfaction
@@ -50,7 +66,9 @@ class FactoryCodelet(Codelet):
         self.result = CodeletResult.SUCCESS
         return self.result
 
-    def _engender_follow_up(self, action_type: Concept, structure_type: Concept):
+    def _engender_follow_up(self):
+        action_type = self.bubble_chamber.spaces["activities"].contents.get_active()
+        structure_type = action_type.links_out.get_active().end
         if action_type == self.bubble_chamber.concepts["build"]:
             if structure_type == self.bubble_chamber.concepts["chunk"]:
                 target = self.bubble_chamber.chunks.get_unhappy()
@@ -110,7 +128,72 @@ class FactoryCodelet(Codelet):
                     target_correspondence.unhappiness,
                 )
         elif action_type == self.bubble_chamber.concepts["evaluate"]:
-            pass
+            if structure_type == self.bubble_chamber.concepts["chunk"]:
+                target = self.bubble_chamber.chunks.get_active()
+                follow_up = ChunkEvaluator.spawn(
+                    self.codelet_id, self.bubble_chamber, target, target.activation
+                )
+            elif structure_type == self.bubble_chamber.concepts["correspondence"]:
+                target = self.bubble_chamber.correspondences.get_active()
+                follow_up = CorrespondenceEvaluator.spawn(
+                    self.codelet_id, self.bubble_chamber, target, target.activation
+                )
+            elif structure_type == self.bubble_chamber.concepts["label"]:
+                target = self.bubble_chamber.labels.get_active()
+                follow_up = LabelEvaluator.spawn(
+                    self.codelet_id, self.bubble_chamber, target, target.activation
+                )
+            elif structure_type == self.bubble_chamber.concepts["relation"]:
+                target = self.bubble_chamber.relations.get_active()
+                follow_up = RelationEvaluator.spawn(
+                    self.codelet_id, self.bubble_chamber, target, target.activation
+                )
+            elif structure_type == self.bubble_chamber.concepts["view"]:
+                target = self.bubble_chamber.views.get_active()
+                follow_up = ViewEvaluator.spawn(
+                    self.codelet_id, self.bubble_chamber, target, target.activation
+                )
         elif action_type == self.bubble_chamber.concepts["select"]:
-            pass
+            if structure_type == self.bubble_chamber.concepts["chunk"]:
+                champion = self.bubble_chamber.chunks.get_active()
+                target_space = champion.parent_spaces.get_random()
+                follow_up = ChunkSelector.spawn(
+                    self.codelet_id,
+                    self.bubble_chamber,
+                    target_space,
+                    champion,
+                    champion.activation,
+                )
+            elif structure_type == self.bubble_chamber.concepts["correspondence"]:
+                champion = self.bubble_chamber.correspondences.get_active()
+                follow_up = CorrespondenceSelector.spawn(
+                    self.codelet_id,
+                    self.bubble_chamber,
+                    champion,
+                    champion.activation,
+                )
+            elif structure_type == self.bubble_chamber.concepts["label"]:
+                champion = self.bubble_chamber.labels.get_active()
+                follow_up = LabelSelector.spawn(
+                    self.codelet_id,
+                    self.bubble_chamber,
+                    champion,
+                    champion.activation,
+                )
+            elif structure_type == self.bubble_chamber.concepts["relation"]:
+                champion = self.bubble_chamber.relations.get_active()
+                follow_up = RelationSelector.spawn(
+                    self.codelet_id,
+                    self.bubble_chamber,
+                    champion,
+                    champion.activation,
+                )
+            elif structure_type == self.bubble_chamber.concepts["view"]:
+                champion = self.bubble_chamber.views.get_active()
+                follow_up = ViewSelector.spawn(
+                    self.codelet_id,
+                    self.bubble_chamber,
+                    champion,
+                    champion.activation,
+                )
         self.child_codelets.append(follow_up)
