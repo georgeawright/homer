89d39b7afb6b915980f7f2c511fe8dec4663bc9d
George Wright <george.alexander.wright@gmail.com>
2020-11-25 11:48:03 +0000

    implement label nearby

--- homer/structures/links/label.py
+++ homer/structures/links/label.py
@@ -1,5 +1,6 @@
 from homer.float_between_one_and_zero import FloatBetweenOneAndZero
 from homer.structure import Structure
+from homer.structure_collection import StructureCollection
 from homer.structures import Concept, Link, Space
 
 
@@ -21,4 +22,16 @@ class Label(Link):
             quality,
             links_in=None,
             links_out=None,
+        )
+
+    def nearby(self, space: Space = None) -> StructureCollection:
+        nearby_chunks = self.start.nearby(self.parent_space)
+        return StructureCollection.difference(
+            StructureCollection.union(
+                StructureCollection(
+                    {label for chunk in nearby_chunks for label in chunk.labels}
+                ),
+                self.start.labels,
+            ),
+            StructureCollection({self}),
         )
---
+++ tests/unit/structures/links/test_label.py
@@ -0,0 +1,19 @@
+from unittest.mock import Mock
+
+from homer.structure_collection import StructureCollection
+from homer.structures.links import Label
+
+
+def test_nearby():
+    label_1 = Mock()
+    chunk_1 = Mock()
+    chunk_1.labels = StructureCollection({label_1})
+    label_2 = Mock()
+    chunk_2 = Mock()
+    chunk_2.labels = StructureCollection({label_2})
+    nearby_chunks = StructureCollection({chunk_1, chunk_2})
+    start = Mock()
+    start.nearby.return_value = nearby_chunks
+    label = Label(start, Mock(), Mock(), Mock())
+    start.labels = StructureCollection({label})
+    assert StructureCollection({label_1, label_2}) == label.nearby()
