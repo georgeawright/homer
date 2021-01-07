abf55695e21ee0c5b984dcbd189c462de7443c29
George Wright <george.alexander.wright@gmail.com>
2021-01-07 14:20:11 +0000

    hack factory codelet to prevent too many of one type of codelet

--- homer/codelets/factory_codelet.py
+++ homer/codelets/factory_codelet.py
@@ -75,7 +75,7 @@ class FactoryCodelet(Codelet):
         return self.result
 
     def _engender_follow_up(self):
-        action_type = self.bubble_chamber.spaces["activities"].contents.get_active()
+        action_type = self.bubble_chamber.spaces["activities"].contents.get_random()
         links_to_structure_nodes = StructureCollection(
             {
                 link
@@ -83,10 +83,10 @@ class FactoryCodelet(Codelet):
                 if link.end in self.bubble_chamber.spaces["structures"].contents
             }
         )
-        structure_type = links_to_structure_nodes.get_active().end
+        structure_type = links_to_structure_nodes.get_random().end
         follow_up_type = self._get_follow_up_type(action_type, structure_type)
         proportion_of_follow_up_type_on_coderack = (
-            self.coderack.proportion_of_codelets_of_type(follow_up_type)
+            self.coderack.number_of_codelets_of_type(follow_up_type) / 50
         )
         if proportion_of_follow_up_type_on_coderack < random.random():
             follow_up = follow_up_type.make(self.codelet_id, self.bubble_chamber)
