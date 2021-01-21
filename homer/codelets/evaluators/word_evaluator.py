2dc617c563e179ad3e3f2a343d9bc2e790fe94fe
George Wright <george.alexander.wright@gmail.com>
2021-01-21 17:50:45 +0000

    implement word evaluator

--- homer/codelets/evaluators/word_evaluator.py
+++ homer/codelets/evaluators/word_evaluator.py
@@ -1,5 +1,75 @@
+from homer.bubble_chamber import BubbleChamber
 from homer.codelets.evaluator import Evaluator
+from homer.codelets.selectors import LabelSelector
+from homer.float_between_one_and_zero import FloatBetweenOneAndZero
+from homer.id import ID
+from homer.structure_collection import StructureCollection
+from homer.structures.chunks import Word
+from homer.structures.links import Label
 
 
 class WordEvaluator(Evaluator):
-    pass
+    def __init__(
+        self,
+        codelet_id: str,
+        parent_id: str,
+        bubble_chamber: BubbleChamber,
+        target_structure: Word,
+        urgency: FloatBetweenOneAndZero,
+    ):
+        Evaluator.__init__(
+            self, codelet_id, parent_id, bubble_chamber, target_structure, urgency
+        )
+        self.original_confidence = self.target_structure.quality
+
+    @classmethod
+    def spawn(
+        cls,
+        parent_id: str,
+        bubble_chamber: BubbleChamber,
+        target_structure: Word,
+        urgency: FloatBetweenOneAndZero,
+    ):
+        codelet_id = ID.new(cls)
+        return cls(codelet_id, parent_id, bubble_chamber, target_structure, urgency)
+
+    @classmethod
+    def make(cls, parent_id: str, bubble_chamber: BubbleChamber):
+        structure_type = bubble_chamber.concepts["word"]
+        target = bubble_chamber.words.get_random()
+        return cls.spawn(parent_id, bubble_chamber, target, structure_type.activation)
+
+    @property
+    def _parent_link(self):
+        structure_concept = self.bubble_chamber.concepts["word"]
+        return structure_concept.relations_with(self._evaluate_concept).get_random()
+
+    def _calculate_confidence(self):
+        if self.target_structure.concepts is None:
+            self.confidence = 1.0
+        else:
+            labels = StructureCollection.union(
+                *[
+                    correspondee.labels
+                    for correspondee in self.target_structure.correspondees
+                ]
+            )
+            compatible_labels = [
+                label
+                for label in labels
+                if label.parent_concept in self.target_structure.concepts
+            ]
+            self.confidence = max(label.quality for label in compatible_labels)
+        self.change_in_confidence = abs(self.confidence - self.original_confidence)
+
+    def _engender_follow_up(self):
+        from homer.codelets.selectors import WordSelector
+
+        self.child_codelets.append(
+            WordSelector.spawn(
+                self.codelet_id,
+                self.bubble_chamber,
+                self.target_structure,
+                self.change_in_confidence,
+            )
+        )
--- homer/codelets/selectors/word_selector.py
+++ homer/codelets/selectors/word_selector.py
@@ -1,5 +1,57 @@
+from homer.bubble_chamber import BubbleChamber
+from homer.codelets.builders import ChunkBuilder
 from homer.codelets.selector import Selector
+from homer.errors import MissingStructureError
+from homer.float_between_one_and_zero import FloatBetweenOneAndZero
+from homer.id import ID
+from homer.structure_collection import StructureCollection
+from homer.structures import Chunk, Space
 
 
 class WordSelector(Selector):
-    pass
+    def __init__(
+        self,
+        codelet_id: str,
+        parent_id: str,
+        bubble_chamber: BubbleChamber,
+        champion: Chunk,
+        urgency: FloatBetweenOneAndZero,
+    ):
+        Selector.__init__(self, codelet_id, parent_id, bubble_chamber, urgency)
+        self.champion = champion
+        self.challenger = None
+
+    @classmethod
+    def spawn(
+        cls,
+        parent_id: str,
+        bubble_chamber: BubbleChamber,
+        champion: Chunk,
+        urgency: FloatBetweenOneAndZero,
+    ):
+        codelet_id = ID.new(cls)
+        return cls(
+            codelet_id,
+            parent_id,
+            bubble_chamber,
+            champion,
+            urgency,
+        )
+
+    @classmethod
+    def make(cls, parent_id: str, bubble_chamber: BubbleChamber):
+        champion = bubble_chamber.words.get_active()
+        return cls.spawn(parent_id, bubble_chamber, champion, champion.activation)
+
+    @property
+    def _structure_concept(self):
+        return self.bubble_chamber.concepts["word"]
+
+    def _passes_preliminary_checks(self):
+        return True
+
+    def _fizzle(self):
+        pass
+
+    def _engender_follow_up(self):
+        pass
