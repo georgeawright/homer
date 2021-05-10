372ec6d3bf48500d6d6c68328f723665a5c9f2de
George Wright <george.alexander.wright@gmail.com>
2021-05-10 17:49:26 +0100

    implement publisher codelet

--- homer/codelets/publisher.py
+++ homer/codelets/publisher.py
@@ -1,10 +1,32 @@
+import random
+
+from homer.bubble_chamber import BubbleChamber
 from homer.codelet import Codelet
+from homer.codelet_result import CodeletResult
+from homer.float_between_one_and_zero import FloatBetweenOneAndZero
 
 
-# TODO
 class Publisher(Codelet):
-    """Takes Output View of a CheckingView as target.
-    The more of the input space that is contained in the ouptut space,
-    and the higher the quality of the output space,
-    the more likely publisher is to succeed.
-    If successful, output is sent to bubble_chamber.result as string"""
+    def __init__(
+        self,
+        codelet_id: str,
+        parent_id: str,
+        bubble_chamber: BubbleChamber,
+        urgency: FloatBetweenOneAndZero,
+    ):
+        Codelet.__init__(self, codelet_id, parent_id, urgency)
+        self.bubble_chamber = bubble_chamber
+
+    def run(self) -> CodeletResult:
+        target_view = self.bubble_chamber.monitoring_views.get_active()
+        if (
+            target_view.quality > random.random()
+            and target_view.activation > random.random()
+        ):
+            words = list(target_view.output_space.contents.where(is_word=True))
+            words.sort(key=lambda word: word.location.coordinates[0][0])
+            text = " ".join([word.value for word in words])
+            self.bubble_chamber.result = text
+            self.result = CodeletResult.SUCCESS
+        else:
+            self.result = CodeletResult.FAIL
---
+++ tests/unit/codelets/test_publisher.py
@@ -0,0 +1,47 @@
+import pytest
+import random
+from unittest.mock import Mock, patch
+
+from homer.codelet_result import CodeletResult
+from homer.codelets import Publisher
+from homer.structure_collection import StructureCollection
+
+
+@pytest.mark.parametrize(
+    "view_quality, view_activation, random_number, success_or_fail",
+    [
+        (1, 1, 0, CodeletResult.SUCCESS),
+        (0.5, 0.5, 0, CodeletResult.SUCCESS),
+    ],
+)
+def test_publishes_text_from_high_quality_active_monitoring_view(
+    view_quality, view_activation, random_number, success_or_fail
+):
+    hello = Mock()
+    hello.is_word = True
+    hello.value = "hello"
+    hello.location.coordinates = [[0]]
+    world = Mock()
+    world.is_word = True
+    world.value = "world"
+    world.location.coordinates = [[1]]
+
+    output_space = Mock()
+    output_space.contents = StructureCollection({hello, world})
+    view = Mock()
+    view.output_space = output_space
+    view.quality = view_quality
+    view.activation = view_activation
+
+    bubble_chamber = Mock()
+    bubble_chamber.result = None
+    bubble_chamber.monitoring_views = StructureCollection({view})
+
+    with patch.object(random, "random", return_value=random_number):
+        publisher = Publisher("", "", bubble_chamber, 1)
+        publisher.run()
+        assert success_or_fail == publisher.result
+        if success_or_fail == CodeletResult.SUCCESS:
+            assert "hello world" == bubble_chamber.result
+        else:
+            assert "" == bubble_chamber.result
