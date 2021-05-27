a2b602d6a7fd6b89f1b47d46b4594673922d203f
George Wright <george.alexander.wright@gmail.com>
2021-05-27 16:20:31 +0100

    remove unlinkedness and unhappiness tests

--- tests/unit/test_structure.py
+++ tests/unit/test_structure.py
@@ -7,40 +7,6 @@ from homer.structures.links import Correspondence, Label, Relation
 from homer.structures.nodes import Lexeme
 
 
-def test_unlinkedness():
-    structure = Structure(Mock(), Mock(), Mock(), Mock())
-    assert 1 == structure.unlinkedness
-    link_in = Mock()
-    link_in.activation = 1
-    structure.links_in.add(link_in)
-    assert 0.5 == structure.unlinkedness
-    link_out = Mock()
-    link_out.activation = 1
-    structure.links_out.add(link_out)
-    assert 0.25 == structure.unlinkedness
-    link_in_and_out = Mock()
-    link_in_and_out.activation = 1
-    structure.links_in.add(link_in_and_out)
-    structure.links_out.add(link_in_and_out)
-    assert 0.125 == structure.unlinkedness
-    inactive_link = Mock()
-    inactive_link.activation = 0
-    structure.links_out.add(inactive_link)
-    assert 0.125 == structure.unlinkedness
-
-
-@pytest.mark.skip
-def test_unhappiness():
-    structure = Structure(Mock(), Mock(), Mock(), Mock())
-    assert 1 == structure.unhappiness
-    structure.parent_chunks.add(Mock())
-    assert 0.75 == structure.unhappiness
-    structure.links_in.add(Mock())
-    assert 0.5 == structure.unhappiness
-    structure.links_out.add(Mock())
-    assert 0.375 == structure.unhappiness
-
-
 def test_correspondences_returns_correspondences():
     number_of_correspondences = 10
     links_in = StructureCollection()
