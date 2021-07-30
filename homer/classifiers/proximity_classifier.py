be82bf8d2ff62ae4fba62cfb0fca4d1003123fff
George Wright <george.alexander.wright@gmail.com>
2021-07-30 15:44:47 +0100

    implement classify link for proximity classifier

--- homer/classifiers/proximity_classifier.py
+++ homer/classifiers/proximity_classifier.py
@@ -1,3 +1,6 @@
+import statistics
+
+from homer import fuzzy
 from homer.classifier import Classifier
 
 
@@ -5,5 +8,25 @@ class ProximityClassifier(Classifier):
     def __init__(self):
         pass
 
-    def classify(self, **kwargs: dict):
+    def classify_link(self, **kwargs: dict):
         return kwargs["concept"].proximity_to(kwargs["start"])
+
+    def classify_chunk(self, **kwargs: dict):
+        root = kwargs.get("root")
+        child = kwargs.get("child")
+        if root is None and child is not None:
+            return 1.0
+        if child is None or child.is_slot:
+            proximities = [
+                space.proximity_between(member, root) if not member.is_slot else 0
+                for space in root.parent_spaces
+                for member in root.members
+                if space.is_conceptual_space and space.is_basic_level
+            ]
+            return statistics.fmean(proximities) if proximities != [] else 0
+        distances = [
+            space.proximity_between(root, child)
+            for space in root.parent_spaces
+            if space.is_conceptual_space and space.is_basic_level
+        ]
+        return 0.0 if distances == [] else fuzzy.AND(*distances)
