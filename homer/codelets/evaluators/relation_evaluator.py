3181cf996b9a7f6d83631feb307777dcef298fe3
George Wright <george.alexander.wright@gmail.com>
2021-08-03 11:57:03 +0100

    fix relation evaluator

--- homer/codelets/evaluators/relation_evaluator.py
+++ homer/codelets/evaluators/relation_evaluator.py
@@ -28,7 +28,7 @@ class RelationEvaluator(Evaluator):
 
     def _calculate_confidence(self):
         target_relation = self.target_structures.get()
-        self.confidence = target_relation.parent_concept.classifier.classify(
+        self.confidence = target_relation.parent_concept.classifier.classify_link(
             space=target_relation.parent_space,
             concept=target_relation.parent_concept,
             start=target_relation.start,
--- tests/unit/codelets/evaluators/test_relation_evaluator.py
+++ tests/unit/codelets/evaluators/test_relation_evaluator.py
@@ -6,13 +6,12 @@ from homer.codelets.selectors import RelationSelector
 from homer.structure_collection import StructureCollection
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize("current_quality, classification", [(0.75, 0.5), (0.5, 0.75)])
 def test_changes_target_structure_quality(current_quality, classification):
     bubble_chamber = Mock()
     bubble_chamber.concepts = {"evaluate": Mock(), "relation": Mock()}
     concept = Mock()
-    concept.classifier.classify.return_value = classification
+    concept.classifier.classify_link.return_value = classification
     relation = Mock()
     relation.quality = current_quality
     relation.parent_concept = concept
