fbb32684f854e55d8a5eb06c6734c5f52006cba7
George Wright <george.alexander.wright@gmail.com>
2021-12-21 16:05:25 +0000

    fill in slots by building link between dummy and actual parent concept

--- homer/codelets/builders/correspondence_builder.py
+++ homer/codelets/builders/correspondence_builder.py
@@ -94,72 +94,26 @@ class CorrespondenceBuilder(Builder):
         return self.target_view.can_accept_member(self.correspondence)
 
     def _process_structure(self):
-        self.correspondence.structure_id = ID.new(Correspondence)
-        try:
-            if self.correspondence.slot_argument.is_word:
-                # TODO: words will no longer be corresponded, only their labels
-                self._fill_in_word_slot()
-            if self.correspondence.slot_argument.is_label:
-                # TODO: relation needs to be built from slot parent concept to actual concept
-                self._fill_in_label_slot()
-            if self.correspondence.slot_argument.is_relation:
-                # TODO: probably the same as for label
-                self._fill_in_relation_slot()
-        except MissingStructureError:
-            pass
-        self.target_view.add(self.correspondence)
-        self.target_space_one.add(self.correspondence)
-        self.target_space_two.add(self.correspondence)
-        self.target_structure_one.links_in.add(self.correspondence)
-        self.target_structure_one.links_out.add(self.correspondence)
-        self.target_structure_two.links_in.add(self.correspondence)
-        self.target_structure_two.links_out.add(self.correspondence)
-        self.bubble_chamber.correspondences.add(self.correspondence)
-        self.bubble_chamber.logger.log(self.correspondence)
+        if not self.correspondence.slot_argument.parent_concept.is_filled_in:
+            self.bubble_chamber.new_relation(
+                parent_id=self.codelet_id,
+                start=self.correspondence.slot_argument.parent_concept,
+                end=self.correspondence.non_slot_argument.parent_concept,
+                parent_concept=None,
+                locations=[],
+                quality=1.0,
+            )
+        self.bubble_chamber.new_correspondence(
+            parent_id=self.codelet_id,
+            start=self.correspondence.start,
+            end=self.correspondence.end,
+            locations=self.correspondence.locations,
+            parent_concept=self.correspondence.parent_concept,
+            conceptual_space=self.correspondence.conceptual_space,
+            parent_view=self.target_view,
+        )
         self.child_structures = self.bubble_chamber.new_structure_collection(
             self.correspondence
-        )
-
-    def _fill_in_word_slot(self):
-        self.target_view.slot_values[
-            self.correspondence.slot_argument.structure_id
-        ] = self.correspondence.non_slot_argument.lexeme.concepts.get()
-
-    def _fill_in_label_slot(self):
-        self.target_view.slot_values[
-            self.correspondence.slot_argument.structure_id
-        ] = self.correspondence.non_slot_argument.parent_concept
-        self.target_view.slot_values[
-            self.correspondence.slot_argument.start.structure_id
-        ] = (
-            self.correspondence.non_slot_argument.start.location_in_space(
-                self.correspondence.conceptual_space
-            )
-            if self.correspondence.non_slot_argument.start.is_node
-            else self.correspondence.non_slot_argument.start.parent_concept
-        )
-
-    def _fill_in_relation_slot(self):
-        self.target_view.slot_values[
-            self.correspondence.slot_argument.structure_id
-        ] = self.correspondence.non_slot_argument.parent_concept
-        self.target_view.slot_values[
-            self.correspondence.slot_argument.start.structure_id
-        ] = (
-            self.correspondence.non_slot_argument.start.location_in_space(
-                self.correspondence.conceptual_space
-            )
-            if self.correspondence.non_slot_argument.start.is_node
-            else self.correspondence.non_slot_argument.start.parent_concept
-        )
-        self.target_view.slot_values[
-            self.correspondence.slot_argument.end.structure_id
-        ] = (
-            self.correspondence.non_slot_argument.end.location_in_space(
-                self.correspondence.conceptual_space
-            )
-            if self.correspondence.non_slot_argument.end.is_node
-            else self.correspondence.non_slot_argument.end.parent_concept
         )
 
     def _fizzle(self):
