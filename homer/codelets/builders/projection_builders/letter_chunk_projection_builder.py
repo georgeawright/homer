f3c512772f1588bb660f48488f6a78109e18130f
George Wright <george.alexander.wright@gmail.com>
2022-01-10 14:41:56 +0000

    make word choice according to word links

--- homer/codelets/builders/projection_builders/letter_chunk_projection_builder.py
+++ homer/codelets/builders/projection_builders/letter_chunk_projection_builder.py
@@ -2,12 +2,6 @@ from homer.location import Location
 from homer.codelets.builders import ProjectionBuilder
 from homer.structure_collection_keys import activation
 
-# TODO: word choice
-# if not a slot then copy word
-# if slot with relation to neighbour use relation to determine choice (ie comparative ending)
-# if slot with label then use label to determine choice (pos + semantic labels)
-# if slot with correspondence then copy correspondee
-
 
 class LetterChunkProjectionBuilder(ProjectionBuilder):
     @classmethod
@@ -23,21 +17,34 @@ class LetterChunkProjectionBuilder(ProjectionBuilder):
         return self.bubble_chamber.concepts["word"]
 
     def _process_structure(self):
-        if self.target_projectee.is_slot:
-            meaning_concept = self.target_view.slot_values[
-                self.frame_correspondee.structure_id
-            ]
-            grammar_concept = self.target_projectee.labels_in_space(
-                self.bubble_chamber.conceptual_spaces["grammar"]
-            ).get()
-            abstract_word = meaning_concept.letter_chunk_forms(grammar_concept).get(
-                key=activation
+        if not self.target_projectee.is_slot:
+            abstract_word = self.target_projectee.abstract_word
+        elif not self.target_projectee.correspondences.is_empty():
+            abstract_word = self.target_projectee.correspondees.get().abstract_word
+        elif not self.target_projectee.relations.is_empty():
+            relation = self.target_projectee.relations.get()
+            relative = self.target_projectee.relatives.get()
+            abstract_relation = relative.abstract_word.relations.where(
+                parent_concept=relation.parent_concept
             )
-            self.target_view.slot_values[
-                self.target_projectee.structure_id
-            ] = abstract_word.name
+            abstract_word = abstract_relation.arguments.get(
+                exclude=[relative.abstract_word]
+            )
         else:
-            abstract_word = self.target_projectee
+            grammar_label = self.target_projectee.labels.where(
+                parent_space=self.bubble_chamber.conceptual_spaces["grammar"]
+            ).get()
+            grammar_concept = grammar_label.parent_concept
+            meaning_label = self.target_projectee.labels.where_not(
+                parent_space=self.bubble_chamber.conceptual_spaces["grammar"]
+            ).get()
+            meaning_concept = meaning_label.parent_concept
+            abstract_word = (
+                meaning_concept.relations.where(parent_concept=grammar_concept)
+                .get(key=activation)
+                .get()
+                .end
+            )
         output_location = Location(
             self.target_projectee.location.coordinates,
             self.target_view.output_space,
@@ -65,18 +72,3 @@ class LetterChunkProjectionBuilder(ProjectionBuilder):
         self.child_structures = self.bubble_chamber.new_structure_collection(
             word, frame_to_output_correspondence
         )
-        if self.target_projectee.is_slot:
-            non_frame_to_output_correspondence = self.bubble_chamber.new_correspondence(
-                parent_id=self.codelet_id,
-                start=self.non_frame_correspondee,
-                end=word,
-                locations=[
-                    self.non_frame_correspondee.location_in_space(self.non_frame),
-                    word.location,
-                ],
-                parent_concept=self.bubble_chamber.concepts["same"],
-                conceptual_space=self.target_correspondence.conceptual_space,
-                parent_view=self.target_view,
-                quality=0.0,
-            )
-            self.child_structures.add(non_frame_to_output_correspondence)
