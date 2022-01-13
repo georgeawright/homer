8c8c795519735a33e48138114bb3d8dee55d8eb2
George Wright <george.alexander.wright@gmail.com>
2022-01-13 16:54:51 +0000

    get rid of conceptual space update activation test

--- tests/unit/structures/spaces/test_conceptual_space.py
+++ tests/unit/structures/spaces/test_conceptual_space.py
@@ -8,7 +8,6 @@ from homer.structures.nodes import Chunk
 from homer.structures.spaces import ConceptualSpace
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize(
     "coordinates, n_s_coordinates, w_e_coordinates, nw_se_coordinates, ne_sw_coordinates",
     [([[0, 0]], [[0]], [[0]], [[0]], [[2]])],
@@ -139,34 +138,3 @@ def test_add(
     assert chunk.location_in_space(ne_sw_space) == Location(
         ne_sw_coordinates, ne_sw_space
     )
-
-
-@pytest.mark.skip
-@pytest.mark.parametrize(
-    "activation_1, activation_2, activation_3, expected_activation",
-    [(1.0, 1.0, 1.0, 1.0), (0.5, 0.2, 0.0, 0.5), (1.0, 0.0, 0.0, 1.0)],
-)
-def test_update_activation(
-    activation_1, activation_2, activation_3, expected_activation
-):
-    concept_1 = Mock()
-    concept_1.activation = activation_1
-    concept_2 = Mock()
-    concept_2.activation = activation_2
-    concept_3 = Mock()
-    concept_3.activation = activation_3
-    conceptual_space = ConceptualSpace(
-        "",
-        "",
-        "name",
-        Mock(),
-        StructureCollection(Mock(), [concept_1, concept_2, concept_3]),
-        Mock(),
-        Mock(),
-        Mock(),
-        Mock(),
-        Mock(),
-        Mock(),
-    )
-    conceptual_space.update_activation()
-    assert expected_activation == conceptual_space.activation
