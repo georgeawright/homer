de598108a501233a4f73a4cb80761dca6f0113b4
George Wright <george.alexander.wright@gmail.com>
2022-02-28 21:17:08 +0000

    force comparative frame to use temeperature conceptual space

--- program.lisp
+++ program.lisp
@@ -877,14 +877,9 @@ lambda location: [[(c[0]+4-c[1])/2] for c in location.coordinates]
 (def-relation :start np-concept :end nn-frame
   :is_bidirectional True :activation 1.0)
 
-(define space-parent-concept
-  (def-concept :name "" :is_slot True))
-(define conceptual-space
-  (def-conceptual-space :name "" :parent_concept space-parent-concept
-    :no_of_dimensions 1))
 (define label-parent-concept
-  (def-concept :name "" :is_slot True :parent_space conceptual-space
-    :locations (list (Location (list) conceptual-space))))
+  (def-concept :name "" :is_slot True :parent_space temperature-space
+    :locations (list (Location (list (list Nan)) temperature-space))))
 (define relation-parent-concept
   (def-concept :name "" :is_slot True :parent_space more-less-space
     :locations (list (Location (list) more-less-space))))
@@ -892,49 +887,49 @@ lambda location: [[(c[0]+4-c[1])/2] for c in location.coordinates]
   :parent_concept more-concept :activation 1.0)
 (define rp-input
   (def-contextual-space :name "rp[jjr].meaning" :parent_concept input-concept
-    :conceptual_spaces (StructureCollection conceptual-space)))
+    :conceptual_spaces (StructureCollection temperature-space)))
 (define rp-output
   (def-contextual-space :name "rp[jjr].text" :parent_concept text-concept
-    :conceptual_spaces (StructureCollection grammar-space conceptual-space)))
+    :conceptual_spaces (StructureCollection grammar-space temperature-space)))
 (define rp-frame
   (def-frame :name "rp[jjr]"
     :parent_concept rp-concept :parent_frame None
     :sub_frames (StructureCollection)
-    :concepts (StructureCollection space-parent-concept label-parent-concept relation-parent-concept)
+    :concepts (StructureCollection label-parent-concept relation-parent-concept)
     :input_space rp-input :output_space rp-output))
 (define chunk-start
-  (def-chunk :locations (list (Location (list) conceptual-space)
+  (def-chunk :locations (list (Location (list (list Nan)) temperature-space)
 			      (Location (list) rp-input))
     :parent_space rp-input))
 (define chunk-end
-  (def-chunk :locations (list (Location (list) conceptual-space)
+  (def-chunk :locations (list (Location (list (list Nan)) temperature-space)
 			      (Location (list) rp-input))
     :parent_space rp-input))
 (define chunk-end-label
   (def-label :start chunk-end :parent_concept label-parent-concept
-    :locations (list (Location (list) conceptual-space)
+    :locations (list (Location (list (list Nan)) temperature-space)
 		     (Location (list) rp-input))))
 (define relation
   (def-relation :start chunk-start :end chunk-end :parent_concept relation-parent-concept
     :locations (list (Location (list (list Nan)) more-less-space)
-		     (TwoPointLocation (list) (list) conceptual-space)
+		     (TwoPointLocation (list (list Nan)) (list (list Nan)) temperature-space)
 		     (TwoPointLocation (list) (list) rp-input))
-    :conceptual_space conceptual-space))
+    :conceptual_space temperature-space))
 (define jjr-chunk
   (def-letter-chunk :name None
-    :locations (list (Location (list) conceptual-space)
+    :locations (list (Location (list (list Nan)) temperature-space)
 		     (Location (list) grammar-space)
 		     (Location (list) rp-output))
     :parent_space rp-output))
 (define er-chunk
   (def-letter-chunk :name None
-    :locations (list (Location (list) conceptual-space)
+    :locations (list (Location (list (list Nan)) temperature-space)
 		     (Location (list) grammar-space)
 		     (Location (list) rp-output))
     :parent_space rp-output))
 (define jjr-super-chunk
   (def-letter-chunk :name None
-    :locations (list (Location (list) conceptual-space)
+    :locations (list (Location (list (list Nan)) temperature-space)
 		     (Location (list) grammar-space)
 		     (Location (list) rp-output))
     :parent_space rp-output
@@ -946,7 +941,7 @@ lambda location: [[(c[0]+4-c[1])/2] for c in location.coordinates]
 		     (Location (list) rp-output))))
 (define jjr-chunk-meaning-label
   (def-label :start jjr-chunk :parent_concept label-parent-concept
-    :locations (list (Location (list) conceptual-space)
+    :locations (list (Location (list (list Nan)) temperature-space)
 		     (Location (list) rp-output))))
 (define er-chunk-relation
   (def-relation :start jjr-chunk :end er-chunk :parent_concept jjr-concept
