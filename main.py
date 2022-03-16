a32a71a17dd6c6d1e7650e94e5e11c09266d5ab5
George Wright <george.alexander.wright@gmail.com>
2022-03-16 11:28:07 +0000

    put each runs logs in a new directory

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import os
+import time
 from unittest.mock import Mock
 
 from homer import Homer
 from homer.loggers import ActivityLogger, StructureLogger
 
+time_string = str(time.time())
+logs_dir_path = f"logs/{time_string}"
+os.mkdir(logs_dir_path)
+structure_logs_dir_path = f"{logs_dir_path}/structures"
+os.mkdir(structure_logs_dir_path)
 
-activity_stream = open("logs/activity.log", "w")
-satisfaction_stream = open("logs/satisfaction.csv", "w")
+activity_stream = open(f"{logs_dir_path}/activity.log", "w")
+satisfaction_stream = open(f"{logs_dir_path}/satisfaction.csv", "w")
 loggers = {
     "activity": ActivityLogger(activity_stream, satisfaction_stream),
-    "structure": StructureLogger("logs/structures"),
+    "structure": StructureLogger(f"{structure_logs_dir_path}"),
     "errors": Mock(),
 }
 narrator = Homer.setup(loggers, random_seed=1)
