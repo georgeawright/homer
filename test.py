cc3f24679c521443166ab5970dd0092f67ccba5c
George Wright <george.alexander.wright@gmail.com>
2022-04-06 16:19:25 +0100

    reduce test runs to 50 and maximum codelets to 50 000

--- test.py
+++ test.py
@@ -6,29 +6,33 @@ from homer import Homer
 from homer.hyper_parameters import HyperParameters
 from homer.loggers import ActivityLogger, ErrorLogger
 
-time_string = str(time.time())
-logs_dir_path = f"logs/{time_string}"
-os.mkdir(logs_dir_path)
+results_file_name = "results1.txt"
+if results_file_name in os.listdir():
+    print("output file already exists")
+    raise Exception
 
-log_file_name = f"{logs_dir_path}/activity"
-satisfaction_stream = open(f"{logs_dir_path}/satisfaction.csv", "w")
+for i in range(50):
+    time_string = str(time.time())
+    logs_dir_path = f"logs/{time_string}"
+    os.mkdir(logs_dir_path)
 
-error_file_name = f"{logs_dir_path}/errors.log"
-error_stream = open(error_file_name, "w")
+    log_file_name = f"{logs_dir_path}/activity"
+    satisfaction_stream = open(f"{logs_dir_path}/satisfaction.csv", "w")
 
-loggers = {
-    "activity": ActivityLogger(log_file_name, satisfaction_stream),
-    "structure": Mock(),
-    "errors": ErrorLogger(error_stream),
-}
+    error_file_name = f"{logs_dir_path}/errors.log"
+    error_stream = open(error_file_name, "w")
 
-HyperParameters.TESTING = True
-with open("results.txt", "w") as results:
-    for i in range(100):
-        narrator = Homer.setup(loggers, random_seed=i)
-        with open("program.lisp", "r") as f:
-            program = f.read()
-            result = narrator.run_program(program)
-            result["time"] = time_string
-            results.write(str(result))
-            results.write("\n")
+    loggers = {
+        "activity": ActivityLogger(log_file_name, satisfaction_stream),
+        "structure": Mock(),
+        "errors": ErrorLogger(error_stream),
+    }
+
+    HyperParameters.TESTING = True
+    narrator = Homer.setup(loggers, random_seed=i)
+    with open(results_file_name, "a") as results, open("program.lisp", "r") as f:
+        program = f.read()
+        result = narrator.run_program(program)
+        result["time"] = time_string
+        results.write(str(result))
+        results.write("\n")
