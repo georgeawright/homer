3435e480058d6e274620b311389de5043e929a16
George Wright <george.alexander.wright@gmail.com>
2022-04-28 10:49:43 +0100

    remove copy tests

--- tests/unit/structures/links/test_relation.py
+++ tests/unit/structures/links/test_relation.py
@@ -5,37 +5,6 @@ from linguoplotter.structure_collection import StructureCollection
 from linguoplotter.structures.links import Relation
 
 
-def test_copy(bubble_chamber):
-    old_start = Mock()
-    old_end = Mock()
-    new_start = Mock()
-    new_end = Mock()
-    parent_id = "id"
-    relation = Relation(
-        Mock(),
-        Mock(),
-        old_start,
-        old_end,
-        StructureCollection(bubble_chamber, [old_start, old_end]),
-        Mock(),
-        Mock(),
-        [Mock(), Mock()],
-        Mock(),
-        Mock(),
-        Mock(),
-        Mock(),
-        Mock(),
-    )
-    copy = relation.copy(
-        start=new_start, end=new_end, parent_id=parent_id, bubble_chamber=bubble_chamber
-    )
-    assert relation.start == old_start
-    assert relation.end == old_end
-    assert copy.start == new_start
-    assert copy.end == new_end
-    assert copy.parent_id == parent_id
-
-
 def test_nearby(bubble_chamber):
     start = Mock()
     end = Mock()
