ad36c0f31d72dcf9320bf24509088caed3910dcf
George Wright <george.alexander.wright@gmail.com>
2022-07-04 11:31:24 +0100

    fix graph snippet

--- log_tools/graph_snippet.py
+++ log_tools/graph_snippet.py
@@ -1,8 +1,6 @@
 import graphviz
 
-# dot_file_path = "logs/structures/spaces/ContextualSpace28/5000.dot"
-dot_file_path = "logs/structures/concepts_and_frames/5000.dot"
-# dot_file_path = "logs/structures/views/SimplexView8/4521.dot"
+dot_file_path = "logs/1656929153.5948377/structures/concepts_and_frames/18000.dot"
 f = open(dot_file_path, "r")
 dot_code = f.read()
 src = graphviz.Source(dot_code)
