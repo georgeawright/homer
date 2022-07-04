3a07166baa6958915edd92f6b7a6650211f7bf95
George Wright <george.alexander.wright@gmail.com>
2022-07-04 18:47:57 +0100

    don't set anything in the recycle bin as focus

--- linguoplotter/codelets/focus_setter.py
+++ linguoplotter/codelets/focus_setter.py
@@ -34,7 +34,7 @@ class FocusSetter(Codelet):
     def run(self) -> CodeletResult:
         try:
             target_view = self.bubble_chamber.production_views.filter(
-                lambda x: x.unhappiness > 0
+                lambda x: x.unhappiness > 0 and x not in self.bubble_chamber.recycle_bin
             ).get(key=exigency)
             self.bubble_chamber.loggers["activity"].log(
                 self, f"Found target view: {target_view}"
