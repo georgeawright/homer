1a597ff665689f48af801a68e535f11d66e8e84a
George Wright <george.alexander.wright@gmail.com>
2022-08-09 11:07:29 +0100

    implement be frame and test input 1

---
+++ example-programs/weather/frames/s-be.lisp
@@ -0,0 +1,238 @@
+(define space-parent-concept
+  (def-concept :name "" :is_slot True))
+(define conceptual-space
+  (def-conceptual-space :name "" :parent_concept space-parent-concept
+    :possible_instances (StructureCollection temperature-space height-space goodness-space)
+    :no_of_dimensions 1))
+(define conceptual-label-concept
+  (def-concept :name "" :is_slot True :parent_space conceptual-space))
+(define location-concept
+  (def-concept :name "" :is_slot True :parent_space location-space))
+ 
+(define description-sub-frame-input
+  (def-contextual-space :name "description-sub-frame.input" :parent_concept input-concept
+    :conceptual_spaces (StructureCollection conceptual-space)))
+(define description-sub-frame-output
+  (def-contextual-space :name "description-sub-frame.text" :parent_concept text-concept
+    :conceptual_spaces (StructureCollection grammar-space conceptual-space)))
+(define description-sub-frame
+  (def-sub-frame :name "s-be-ap-sub" :parent_concept ap-concept :parent_frame None
+    :sub_frames (StructureCollection)
+    :concepts (StructureCollection conceptual-label-concept)
+    :input_space description-sub-frame-input
+    :output_space description-sub-frame-output))
+
+(define location-sub-frame-input
+  (def-contextual-space :name "location-sub-frame.meaning" :parent_concept input-concept
+    :conceptual_spaces (StructureCollection location-space)))
+(define location-sub-frame-output
+  (def-contextual-space :name "location-sub-frame.text" :parent_concept text-concept
+    :conceptual_spaces (StructureCollection grammar-space location-space)))
+(define location-sub-frame
+  (def-sub-frame :name "s-be-location-sub"
+    :parent_concept pp-inessive-location-concept
+    :parent_frame None
+    :sub_frames (StructureCollection)
+    :concepts (StructureCollection location-concept)
+    :input_space location-sub-frame-input
+    :output_space location-sub-frame-output))
+
+(define time-sub-frame-input
+  (def-contextual-space :name "time-sub-frame.meaning" :parent_concept input-concept
+    :conceptual_spaces (StructureCollection time-space)))
+(define time-sub-frame-output
+  (def-contextual-space :name "time-sub-frame.text" :parent_concept text-concept
+    :conceptual_spaces (StructureCollection grammar-space time-space)))
+(define time-sub-frame
+  (def-sub-frame :name "s-be-time-sub"
+    :parent_concept pp-directional-time-concept
+    :parent_frame None
+    :sub_frames (StructureCollection)
+    :concepts (StructureCollection)
+    :input_space time-sub-frame-input
+    :output_space time-sub-frame-output))
+
+(define be-sentence-input
+  (def-contextual-space :name "s-be.meaning" :parent_concept input-concept
+    :conceptual_spaces (StructureCollection location-space time-space conceptual-space)))
+(define be-sentence-output
+  (def-contextual-space :name "s-be.text" :parent_concept text-concept
+    :conceptual_spaces (StructureCollection
+			grammar-space location-space time-space conceptual-space)))
+(define be-sentence
+  (def-frame :name "s-be" :parent_concept sentence-concept :parent_frame None
+    :depth 6
+    :sub_frames (StructureCollection description-sub-frame location-sub-frame time-sub-frame)
+    :concepts (StructureCollection location-concept)
+    :input_space be-sentence-input
+    :output_space be-sentence-output))
+
+(define early-chunk
+  (def-chunk :locations (list (Location (list (list Nan Nan)) location-space)
+			      (Location (list (list Nan)) time-space)
+			      (Location (list (list Nan)) conceptual-space)
+			      (Location (list) location-sub-frame-input)
+			      (Location (list) time-sub-frame-input)
+			      (Location (list) be-sentence-input))
+    :parent_space be-sentence-input))
+(define early-chunk-conceptual-label
+  (def-label :start early-chunk :parent_concept conceptual-label-concept
+    :locations (list (Location (list) conceptual-space)
+		     (Location (list) description-sub-frame-input)
+		     (Location (list) be-sentence-input))
+    :parent_space description-sub-frame-input))
+(define late-chunk
+  (def-chunk :locations (list (Location (list (list Nan Nan)) location-space)
+			      (Location (list (list Nan)) time-space)
+			      (Location (list (list Nan)) conceptual-space)
+			      (Location (list) location-sub-frame-input)
+			      (Location (list) time-sub-frame-input)
+			      (Location (list) be-sentence-input))
+    :parent_space be-sentence-input))
+(define late-chunk-location-label
+  (def-label :start late-chunk :parent_concept location-concept
+    :locations (list (Location (list (list Nan Nan)) location-space)
+		     (Location (list) location-sub-frame-input)
+		     (Location (list) be-sentence-input))
+    :parent_space location-sub-frame-input))
+(define time-relation
+  (def-relation :start early-chunk :end late-chunk :parent_concept less-concept
+    :quality 1.0
+    :locations (list (Location (list (list Nan)) more-less-space)
+		     (TwoPointLocation (list (list Nan)) (list (list Nan)) time-space)
+		     (TwoPointLocation (list) (list) time-sub-frame-input)
+		     (TwoPointLocation (list) (list) be-sentence-input))
+    :parent_space time-sub-frame-input
+    :conceptual_space time-space))
+(define location-relation
+  (def-relation :start late-chunk :end early-chunk :parent_concept same-concept
+    :quality 1.0
+    :locations (list (Location (list (list Nan)) same-different-space)
+		     (TwoPointLocation (list (list Nan Nan)) (list (list Nan Nan)) location-space)
+		     (TwoPointLocation (list) (list) be-sentence-input))
+    :parent_space be-sentence-input
+    :conceptual_space location-space))
+(define conceptual-relation
+  (def-relation :start late-chunk :end early-chunk :parent_concept same-concept
+    :quality 1.0
+    :locations (list (Location (list (list Nan)) same-different-space)
+		     (TwoPointLocation (list (list Nan)) (list (list Nan)) conceptual-space)
+		     (TwoPointLocation (list) (list) be-sentence-input))
+    :parent_space be-sentence-input
+    :conceptual_space conceptual-space))
+
+(define sentence-word-1
+  (def-letter-chunk :name "temperatures"
+    :locations (list nsubj-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :abstract_chunk temperatures))
+(define sentence-word-1-label
+  (def-label :start sentence-word-1 :parent_concept nsubj-concept
+    :locations (list nsubj-location
+		     (Location (list) be-sentence-output))))
+(define sentence-word-2
+  (def-letter-chunk :name "will"
+    :locations (list vb-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :abstract_chunk will))
+(define sentence-word-3
+  (def-letter-chunk :name "be"
+    :locations (list vb-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :abstract_chunk be))
+(define sentence-word-4
+  (def-letter-chunk :name None
+    :locations (list ap-location
+		     (Location (list) description-sub-frame-output)
+		     (Location (list) be-sentence-output))
+    :parent_space description-sub-frame-output))
+(define description-chunk-grammar-label
+  (def-label :start sentence-word-4 :parent_concept ap-concept
+    :locations (list ap-location
+		     (Location (list) description-sub-frame-output))))
+(define sentence-word-5
+  (def-letter-chunk :name None
+    :locations (list pp-location
+		     (Location (list) location-sub-frame-output)
+		     (Location (list) be-sentence-output))
+    :parent_space location-sub-frame-output))
+(define location-chunk-grammar-label
+  (def-label :start sentence-word-5 :parent_concept pp-inessive-location-concept
+    :locations (list pp-location
+		     (Location (list) location-sub-frame-output))))
+(define sentence-word-6
+  (def-letter-chunk :name None
+    :locations (list pp-location
+		     (Location (list) time-sub-frame-output)
+		     (Location (list) be-sentence-output))
+    :parent_space time-sub-frame-output))
+(define time-chunk-grammar-label
+  (def-label :start sentence-word-6 :parent_concept pp-directional-time-concept
+    :locations (list pp-location
+		     (Location (list) time-sub-frame-output))))
+
+(define v-super-chunk
+  (def-letter-chunk :name None
+    :locations (list v-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :left_branch (StructureCollection sentence-word-2)
+    :right_branch (StructureCollection sentence-word-3)))
+(define v-super-chunk-label
+  (def-label :start v-super-chunk :parent_concept v-concept
+    :locations (list v-location
+		     (Location (list) be-sentence-output))))
+(define pp-super-chunk
+  (def-letter-chunk :name None
+    :locations (list pp-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :left_branch (StructureCollection sentence-word-5)
+    :right_branch (StructureCollection sentence-word-6)))
+(define pp-super-chunk-label
+  (def-label :start pp-super-chunk :parent_concept pp-concept
+    :locations (list pp-location
+		     (Location (list) be-sentence-output))))
+(define pred-super-chunk
+  (def-letter-chunk :name None
+    :locations (list predicate-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :left_branch (StructureCollection sentence-word-4)
+    :right_branch (StructureCollection pp-super-chunk)))
+(define pred-super-chunk-label
+  (def-label :start pred-super-chunk :parent_concept predicate-concept
+    :locations (list predicate-location
+		     (Location (list) be-sentence-output))))
+(define vp-super-chunk
+  (def-letter-chunk :name None
+    :locations (list vp-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :left_branch (StructureCollection v-super-chunk)
+    :right_branch (StructureCollection pred-super-chunk)))
+(define vp-super-chunk-label
+  (def-label :start vp-super-chunk :parent_concept vp-concept
+    :locations (list vp-location
+		     (Location (list) be-sentence-output))))
+(define sentence-super-chunk
+  (def-letter-chunk :name None
+    :locations (list sentence-location
+		     (Location (list) be-sentence-output))
+    :parent_space be-sentence-output
+    :left_branch (StructureCollection sentence-word-1)
+    :right_branch (StructureCollection vp-super-chunk)))
+(define sentence-super-chunk-label
+  (def-label :start sentence-super-chunk :parent_concept sentence-concept
+    :locations (list sentence-location
+		     (Location (list) be-sentence-output))))
+
+(def-relation :start ap-concept :end be-sentence
+  :is_bidirectional True :activation 1.0)
+(def-relation :start pp-inessive-location-concept :end be-sentence
+  :is_bidirectional True :activation 1.0)
+(def-relation :start pp-directional-time-concept :end be-sentence
+  :is_bidirectional True :activation 1.0)
---
+++ example-programs/weather/narration-1.lisp
@@ -0,0 +1,384 @@
+(load "conceptual-spaces/grammar.lisp")
+(load "conceptual-spaces/same-different.lisp")
+(load "conceptual-spaces/more-less.lisp")
+(load "conceptual-spaces/magnitude.lisp")
+(load "conceptual-spaces/height.lisp")
+(load "conceptual-spaces/goodness.lisp")
+(load "conceptual-spaces/size.lisp")
+(load "conceptual-spaces/lateness.lisp")
+(load "conceptual-spaces/extremeness.lisp")
+(load "conceptual-spaces/temperature.lisp")
+(load "conceptual-spaces/peripheralness.lisp")
+(load "conceptual-spaces/location.lisp")
+(load "conceptual-spaces/time.lisp")
+
+(load "frames/ap-jj.lisp")
+
+(load "frames/pp-inessive-time.lisp")
+(load "frames/pp-between-times.lisp")
+(load "frames/pp-inessive-location.lisp")
+(load "frames/pp-from-to-different-locations.lisp")
+(load "frames/pp-from-wards-locations.lisp")
+(load "frames/pp-from-to-locations.lisp")
+
+(load "frames/s-be.lisp")
+(load "frames/s-increase.lisp")
+(load "frames/s-move.lisp")
+(load "frames/s-expand.lisp")
+(load "frames/s-spread.lisp")
+(load "frames/s-and.lisp")
+
+(define input-space
+  (def-contextual-space
+    :name "input" :parent_concept input-concept :is_main_input True
+    :conceptual_spaces (StructureCollection
+			temperature-space location-space time-space)))
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 3)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 1 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 1 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 4)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 1 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 1 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 3 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 3 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 3 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 4)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 3 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 5 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 5 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 5 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 5 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 7 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 7 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 7 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 0)) time-space)
+		   (Location (list (list 7 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 1 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 1 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 1 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 1 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 7)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 3 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 3 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 3 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 3 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 8)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 5 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 5 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 7)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 5 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 5 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 7)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 7 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 8)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 7 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 7)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 7 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 7)) temperature-space)
+		   (Location (list (list 24)) time-space)
+		   (Location (list (list 7 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 4)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 1 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 1 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 4)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 1 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 1 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 3 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 3 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 3 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 3 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 5 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 5 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 5 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 5 7)) location-space))
+  :parent_space input-space)
+
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 7 1)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 6)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 7 3)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 7 5)) location-space))
+  :parent_space input-space)
+(def-chunk
+  :is_raw True
+  :locations (list (Location (list) input-space)
+		   (Location (list (list 5)) temperature-space)
+		   (Location (list (list 48)) time-space)
+		   (Location (list (list 7 7)) location-space))
+  :parent_space input-space)
+
