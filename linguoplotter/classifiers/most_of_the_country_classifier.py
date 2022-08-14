4522556bc9af1d0999aa60885c83b25dd662f0df
George Wright <george.alexander.wright@gmail.com>
2022-08-14 12:24:36 +0100

    most of the country classifier

---
+++ linguoplotter/classifiers/most_of_the_country_classifier.py
@@ -0,0 +1,13 @@
+from linguoplotter.classifier import Classifier
+
+
+class MostOfTheCountryClassifier(Classifier):
+    def classify(self, **kwargs: dict):
+        """
+        Required: 'concept'; 'start' OR 'collection'
+        """
+        collection = kwargs.get("collection")
+        start = kwargs.get("start")
+        item = start if start is not None else collection.get()
+
+        return 0.5 ** abs(13 - item.size)
