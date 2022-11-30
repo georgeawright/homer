87bdae88a9b0f51d9f4e13e20d855a753d840925
George Wright <george.alexander.wright@gmail.com>
2022-11-30 10:41:35 +0000

    label and relation suggestion depends on qualities divided by conceptual spaces - more even performance between problems

--- linguoplotter/codelets/factories/bottom_up_factories/bottom_up_suggester_factory.py
+++ linguoplotter/codelets/factories/bottom_up_factories/bottom_up_suggester_factory.py
@@ -1,3 +1,5 @@
+import statistics
+
 from linguoplotter.codelets.factories import BottomUpFactory
 from linguoplotter.codelets.suggesters import (
     ChunkSuggester,
@@ -7,32 +9,32 @@ from linguoplotter.codelets.suggesters import (
 )
 from linguoplotter.codelets.suggesters import ViewSuggester
 from linguoplotter.errors import MissingStructureError
-from linguoplotter.float_between_one_and_zero import FloatBetweenOneAndZero
+from linguoplotter.structure_collections import StructureSet
 
 
 class BottomUpSuggesterFactory(BottomUpFactory):
     def _engender_follow_up(self):
-        proportion_of_unchunked_raw_chunks = self._proportion_of_unchunked_raw_chunks()
-        proportion_of_unlabeled_chunks = self._proportion_of_unlabeled_chunks()
-        proportion_of_unrelated_chunks = self._proportion_of_unrelated_chunks()
-        proportion_of_uncorresponded_links = self._proportion_of_uncorresponded_links()
-        proportion_of_unfilled_slots = self._proportion_of_unfilled_slots()
+        unchunkedness = self._unchunkedness_of_raw_chunks()
+        unlabeledness = self._unlabeledness_of_chunks()
+        unrelatedness = self._unrelatedness_of_chunks()
+        uncorrespondedness = self._uncorrespondedness_of_links()
+        unfilledness = self._unfilledness_of_slots()
 
         self.bubble_chamber.loggers["activity"].log(
-            f"Proportion of unchunked raw chunks: {proportion_of_unchunked_raw_chunks}\n"
-            + f"Proportion of unlabeled chunks: {proportion_of_unlabeled_chunks}\n"
-            + f"Proportion of unrelated chunks: {proportion_of_unrelated_chunks}\n"
-            + f"Proportion of uncorresponded links: {proportion_of_uncorresponded_links}\n"
-            + f"Proportion of unfilled_slots: {proportion_of_unfilled_slots}",
+            f"Proportion of unchunked raw chunks: {unchunkedness}\n"
+            + f"Proportion of unlabeled chunks: {unlabeledness}\n"
+            + f"Proportion of unrelated chunks: {unrelatedness}\n"
+            + f"Proportion of uncorresponded links: {uncorrespondedness}\n"
+            + f"Proportion of unfilled_slots: {unfilledness}",
         )
 
         follow_up_class = self.bubble_chamber.random_machine.select(
             [
-                (ChunkSuggester, proportion_of_unchunked_raw_chunks),
-                (LabelSuggester, proportion_of_unlabeled_chunks),
-                (RelationSuggester, proportion_of_unrelated_chunks),
-                (CorrespondenceSuggester, proportion_of_unfilled_slots),
-                (ViewSuggester, proportion_of_uncorresponded_links),
+                (ChunkSuggester, unchunkedness),
+                (LabelSuggester, unlabeledness),
+                (RelationSuggester, unrelatedness),
+                (ViewSuggester, uncorrespondedness),
+                (CorrespondenceSuggester, unfilledness),
             ],
             key=lambda x: x[1],
         )[0]
@@ -41,44 +43,35 @@ class BottomUpSuggesterFactory(BottomUpFactory):
             follow_up_class.make(self.codelet_id, self.bubble_chamber)
         )
 
-    def _proportion_of_unchunked_raw_chunks(self):
+    def _unchunkedness_of_raw_chunks(self):
         input_space = self.bubble_chamber.spaces.where(is_main_input=True).get()
         raw_chunks = input_space.contents.filter(
             lambda x: x.is_chunk and x.is_raw
         ).sample(10)
         return len(raw_chunks.where(unchunkedness=1.0)) / len(raw_chunks)
 
-    def _proportion_of_unlabeled_chunks(self):
+    def _unlabeledness_of_chunks(self):
         input_space = self.bubble_chamber.spaces.where(is_main_input=True).get()
         try:
-            non_raw_chunks = input_space.contents.filter(
-                lambda x: x.is_chunk and not x.is_raw
-            ).sample(10)
-            total_label_quality = sum(
-                label.quality for chunk in non_raw_chunks for label in chunk.labels
-            )
-            return 1 - FloatBetweenOneAndZero(total_label_quality / len(non_raw_chunks))
-        except MissingStructureError:
-            return float("-inf")
-
-    def _proportion_of_unrelated_chunks(self):
-        input_space = self.bubble_chamber.spaces.where(is_main_input=True).get()
-        try:
-            non_raw_chunks = input_space.contents.filter(
-                lambda x: x.is_chunk and not x.is_raw
-            ).sample(10)
-            total_relation_quality = sum(
-                relation.quality
-                for chunk in non_raw_chunks
-                for relation in chunk.relations
-            )
-            return 1 - FloatBetweenOneAndZero(
-                total_relation_quality / len(non_raw_chunks)
+            chunk = input_space.contents.where(is_chunk=True, is_raw=False).get()
+            return 1 - sum([l.quality for l in chunk.labels]) / len(
+                input_space.conceptual_spaces
             )
         except MissingStructureError:
             return float("-inf")
 
-    def _proportion_of_uncorresponded_links(self):
+    def _unrelatedness_of_chunks(self):
+        input_space = self.bubble_chamber.spaces.where(is_main_input=True).get()
+        try:
+            chunks = input_space.contents.where(is_chunk=True, is_raw=False).sample(2)
+            relations = StructureSet.intersection(*[c.relations for c in chunks])
+            return 1 - sum([r.quality for r in relations]) / len(
+                input_space.conceptual_spaces
+            )
+        except MissingStructureError:
+            return float("-inf")
+
+    def _uncorrespondedness_of_links(self):
         input_space = self.bubble_chamber.spaces.where(is_main_input=True).get()
         try:
             labels_and_relations = input_space.contents.filter(
@@ -91,7 +84,7 @@ class BottomUpSuggesterFactory(BottomUpFactory):
         except MissingStructureError:
             return float("-inf")
 
-    def _proportion_of_unfilled_slots(self):
+    def _unfilledness_of_slots(self):
         try:
             views = self.bubble_chamber.views.sample(10)
             unfilled_slots = sum(len(view.unfilled_input_structures) for view in views)
