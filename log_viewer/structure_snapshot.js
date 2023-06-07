de51e1af330e7a8d3933297e6e26d8bc436289ef
George Wright <george.alexander.wright@gmail.com>
2023-06-07 09:12:29 +0100

    move svg graph to top of snapshot pages and tidy code

--- log_viewer/structure_snapshot.js
+++ log_viewer/structure_snapshot.js
@@ -2,12 +2,12 @@ var fs = require('fs');
 var tools = require('./tools');
 
 exports.run = function(query) {
-    var run_id = query.run_id;
-    var structure_id = query.structure_id;
-    var time = query.time
-    var structure_directory = 'logs/' + query.run_id
-	+ '/structures/structures/' + structure_id;
-    var structure_files = fs.readdirSync(structure_directory);
+    const run_id = query.run_id;
+    const structure_id = query.structure_id;
+    const time = query.time
+    const structure_directory = `logs/${run_id}/structures/structures/${structure_id}`;
+    const structure_files = fs.readdirSync(structure_directory);
+
     var structure_file = '';
     var latest_time = -1;
     structure_files.forEach(file => {
@@ -17,27 +17,26 @@ exports.run = function(query) {
 	    latest_time = file_time;
 	}
     });
-    var log_file = structure_directory + '/' + structure_file;
 
-    var doc = '<html><body>';
-    doc += '<p><a href="../">&lt;&lt;All runs</a></p>';
-    doc += '<h1><a href="run?run_id=' + run_id + '">Linguoplotter run '+run_id+'</a></h1>';
-    doc += '<p><a href="codelets?run_id=' + run_id + '">Codelets</a></p>';
-    doc += '<p><a href="structures?run_id=' + run_id + '">Structures</a></p>';
+    const graph_file = tools.get_graph(run_id, structure_id, time);
+    const graph_svg = fs.readFileSync(graph_file);
 
-    var structure_url = 'structure_lifetime?run_id=' + run_id + '&structure_id=' + structure_id;
-    doc += '<h2><a href="' + structure_url + '">' + structure_id + '</a></h2>'
+    const log_file = structure_directory + '/' + structure_file;
+    const structure_json = JSON.parse(fs.readFileSync(log_file));
+    const structure_html = tools.json_to_html(structure_json, query);
 
-    structure_json = JSON.parse(fs.readFileSync(log_file));
-    console.log(structure_json);
-    structure_html = tools.json_to_html(structure_json, query);
-
-    doc += structure_html;
-
-    graph_file = tools.get_graph(run_id, structure_id, time);
-    graph_svg = fs.readFileSync(graph_file);
-    doc += `<object type="image/svg+xml">${graph_svg}</object>`;
-
-    doc += '</body></html>';
+    const doc = `
+<html>
+  <body>
+    <p><a href="../">&lt;&lt;All runs</a></p>
+    <h1><a href="run?run_id=${run_id}">Linguoplotter run ${run_id}</a></h1>
+    <p><a href="codelets?run_id=${run_id}">Codelets</a></p>
+    <p><a href="structures?run_id=${run_id}">Structures</a></p>
+    <h2><a href="structure_lifetime?run_id=${run_id}&structure_id=${structure_id}">${structure_id}</a></h2>
+    <object type="image/svg+xml">${graph_svg}</object>
+    ${structure_html}
+  </body>
+</html>
+`;
     return doc;
 }
