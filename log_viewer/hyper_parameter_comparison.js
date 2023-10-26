bd27592030b552a431130f093d9380ca6b7a9dad
George Alexander Wright <george.alexander.wright@gmail.com>
2023-10-26 22:21:16 +0100

    hyperparameters comparison

--- log_viewer/hyper_parameter_comparison.js
+++ log_viewer/hyper_parameter_comparison.js
@@ -10,7 +10,7 @@ exports.run = function(query) {
     hyperparams_programs_results = {};
     directories = [];
     fs.readdirSync('logs/').forEach(directory => {
-	if (directory == "stats.txt" || directory ==  "texts.csv") {
+	if (directory == "stats.txt" || directory == "stats1.txt" || directory ==  "texts.csv") {
 	    return;
 	}
 	directories.push(directory);
@@ -37,13 +37,19 @@ exports.run = function(query) {
 	Object.keys(hyperparams_programs_results[hyperparams]).forEach(program => {
 	    program_results = hyperparams_programs_results[hyperparams][program];
 	    satisfactions = [];
+	    non_null_satisfactions = [];
 	    run_lengths = [];
 	    null_count = 0;
 	    program_results.forEach(result => {
+		if (result["satisfaction"] === undefined) {
+		    return;
+		}
 		satisfactions.push(result["satisfaction"]);
 		run_lengths.push(result["codelets_run"]);
 		if (result["output"] === null) {
 		    null_count += 1;
+		} else {
+		    non_null_satisfactions.push(result["satisfaction"])
 		}
 	    });
 	    hyperparams_programs_table[hyperparams][program] = {
@@ -53,6 +59,7 @@ exports.run = function(query) {
 		"mean_run_length": mathjs.mean(run_lengths),
 		"median_run_length": mathjs.median(run_lengths),
 		"timeout_rate": null_count / satisfactions.length,
+		"non_null_satisfaction": non_null_satisfactions,
 	    };
 	});
     });
@@ -65,6 +72,7 @@ exports.run = function(query) {
 	mean_run_lengths = [];
 	median_run_lengths = [];
 	timeout_rates = [];
+	non_null_satisfactions = [];
 	Object.keys(hyperparams_programs_table[hyperparams]).forEach(program => {
 	    p = hyperparams_programs_table[hyperparams][program];
 	    mean_satisfactions.push(p["mean_satisfaction"]);
@@ -73,7 +81,10 @@ exports.run = function(query) {
 	    mean_run_lengths.push(p["mean_run_length"]);
 	    median_run_lengths.push(p["median_run_length"]);
 	    timeout_rates.push(p["timeout_rate"]);
+	    non_null_satisfactions = non_null_satisfactions.concat(p["non_null_satisfaction"]);
 	});
+	console.log(hyperparams);
+	console.log(non_null_satisfactions)
 	hyperparams_table[hyperparams] = {
 	    "mean_satisfaction": mathjs.mean(mean_satisfactions),
 	    "median_satisfaction": mathjs.median(median_satisfactions),
@@ -83,6 +94,8 @@ exports.run = function(query) {
 	    "median_run_length": mathjs.median(median_run_lengths),
 	    "mean_timeout_rate": mathjs.mean(timeout_rates),
 	    "median_timeout_rate": mathjs.median(timeout_rates),
+	    "mean_non_null_satisfaction": mathjs.mean(non_null_satisfactions),
+	    "median_non_null_satisfaction": mathjs.median(non_null_satisfactions),
 	}
 	mean_satisfaction = hyperparams_table[hyperparams]["mean_satisfaction"];
 	median_satisfaction = hyperparams_table[hyperparams]["median_satisfaction"];
@@ -92,7 +105,9 @@ exports.run = function(query) {
 	median_run_length = hyperparams_table[hyperparams]["median_run_length"];
 	mean_timeout_rate = hyperparams_table[hyperparams]["mean_timeout_rate"];
 	median_timeout_rate = hyperparams_table[hyperparams]["median_timeout_rate"];
-	doc += `${hyperparams}\t${mean_satisfaction}\t${median_satisfaction}\t${mean_max_satisfaction}\t${median_max_satisfaction}\t${mean_run_length}\t${median_run_length}\t${mean_timeout_rate}\t${median_timeout_rate}\n`;
+	mean_non_null_satisfaction = hyperparams_table[hyperparams]["mean_non_null_satisfaction"];
+	median_non_null_satisfaction = hyperparams_table[hyperparams]["median_non_null_satisfaction"];
+	doc += `${hyperparams}\t${mean_satisfaction}\t${median_satisfaction}\t${mean_max_satisfaction}\t${median_max_satisfaction}\t${mean_run_length}\t${median_run_length}\t${mean_timeout_rate}\t${median_timeout_rate}\t${mean_non_null_satisfaction}\t${median_non_null_satisfaction}\n`;
     });
     return doc;
 }
